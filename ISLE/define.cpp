9c985415872070962a6d7f262124a068e704fdd0
TheEmeraldFalcon <58223546+TheEmeraldFalcon@users.noreply.github.com>
2023-06-10 22:54:45 -0400

    Update define.cpp
    Renamed _DAT_004101bc to g_startDelayTimer.

--- ISLE/define.cpp
+++ ISLE/define.cpp
@@ -4,7 +4,7 @@ Isle *g_isle = 0;
 int g_closed = 0;
 
 const char *WNDCLASS_NAME = "Lego Island MainNoM App";
-const char *WINDOW_TITLE = "LEGO®";
+const char *WINDOW_TITLE = "LEGOÂ®";
 
 unsigned char g_mousedown = 0;
 unsigned char g_mousemoved = 0;
@@ -14,5 +14,5 @@ int g_targetWidth = 640;
 int g_targetHeight = 480;
 unsigned int g_targetDepth = 16;
 int _DAT_00410064 = 0;
-int _DAT_004101bc = 200;
+int g_startDelayTimer = 200;
 long g_lastFrameTime = 0;
