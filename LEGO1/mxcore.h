3d8cc427128e8092e48f8b8acf72e479f429d7d4
Christian Semmler <mail@csemmler.com>
2023-06-25 14:51:05 +0200

    lego: match GetClassName/IsClass

--- LEGO1/mxcore.h
+++ LEGO1/mxcore.h
@@ -1,6 +1,8 @@
 #ifndef MXCORE_H
 #define MXCORE_H
 
+#include <string.h>
+
 #include "mxbool.h"
 
 class MxParam;
@@ -12,8 +14,14 @@ public:
   __declspec(dllexport) virtual ~MxCore(); // vtable+00
   __declspec(dllexport) virtual long Notify(MxParam &p); // vtable+04
   virtual long Tickle(); // vtable+08
-  virtual const char *GetClassName() const; // vtable+0c
-  virtual MxBool IsClass(const char *name) const; // vtable+10
+
+  // OFFSET: LEGO1 0x100144c0
+  inline virtual const char *GetClassName() const { return "MxCore"; }; // vtable+0c
+
+  // OFFSET: LEGO1 0x100140d0
+  inline virtual MxBool IsClass(const char *name) const {
+    return !strcmp(name, MxCore::GetClassName());
+  }; // vtable+10
 
 private:
   unsigned int m_id;
