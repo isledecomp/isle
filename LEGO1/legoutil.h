c994ed7d16a9c347f44110be99d908209302b432
Christian Semmler <mail@csemmler.com>
2023-06-30 19:58:49 +0200

    lego: Implement/match CalculateNewVel

--- LEGO1/legoutil.h
+++ LEGO1/legoutil.h
@@ -7,4 +7,16 @@ inline T Abs(T p_t)
   return p_t < 0 ? -p_t : p_t;
 }
 
+template <class T>
+inline T Min(T p_t1, T p_t2)
+{
+    return p_t1 < p_t2 ? p_t1 : p_t2;
+}
+
+template <class T>
+inline T Max(T p_t1, T p_t2)
+{
+    return p_t1 > p_t2 ? p_t1 : p_t2;
+}
+
 #endif // LEGOUTIL_H
