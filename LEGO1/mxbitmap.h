e02e7c768183d34300ab7065f5f9e4129a8c6afe
Joshua Peisach <itzswirlz2020@outlook.com>
2023-06-30 17:07:41 -0400

    Initial implementation of MxBitmap
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- LEGO1/mxbitmap.h
+++ LEGO1/mxbitmap.h
@@ -1,15 +1,26 @@
 #ifndef MXBITMAP_H
 #define MXBITMAP_H
 
-#include "mxpalette.h"
+#include <stdlib.h>
 
-class MxBitmap
+#include "mxcore.h"
+#include "mxpalette.h"
+#include "mxtypes.h"
+
+class MxBitmap : public MxCore
 {
 public:
   __declspec(dllexport) MxBitmap();
   __declspec(dllexport) virtual ~MxBitmap();
   __declspec(dllexport) virtual MxPalette *CreatePalette();
   __declspec(dllexport) virtual long Read(const char *);
+private:
+  BITMAPINFO *m_info;
+  BITMAPINFOHEADER *m_bmiHeader;
+  RGBQUAD *m_paletteData;
+  LPVOID *m_data;
+  MxBool m_unk18;
+  MxPalette *m_palette;
 };
 
 #endif // MXBITMAP_H
