23bd977c60238023ea406f37ab4d8e397e6017fa
Christian Semmler <mail@csemmler.com>
2023-06-30 20:01:00 +0200

    consistency

--- LEGO1/legoutil.h
+++ LEGO1/legoutil.h
@@ -10,13 +10,13 @@ inline T Abs(T p_t)
 template <class T>
 inline T Min(T p_t1, T p_t2)
 {
-    return p_t1 < p_t2 ? p_t1 : p_t2;
+  return p_t1 < p_t2 ? p_t1 : p_t2;
 }
 
 template <class T>
 inline T Max(T p_t1, T p_t2)
 {
-    return p_t1 > p_t2 ? p_t1 : p_t2;
+  return p_t1 > p_t2 ? p_t1 : p_t2;
 }
 
 #endif // LEGOUTIL_H
