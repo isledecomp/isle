07bf8e19d7272eac4708545d36a6d765ea443084
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-06-30 03:26:50 +0200

    MxVideoPresenter: add members to match size

--- LEGO1/mxvideopresenter.cpp
+++ LEGO1/mxvideopresenter.cpp
@@ -1,1 +1,8 @@
 #include "mxvideopresenter.h"
+
+DECOMP_STATIC_ASSERT(sizeof(MxVideoPresenter) == 100);
+
+// OFFSET: LEGO1 0x100b2760 STUB
+void MxVideoPresenter::Init() {
+  // TODO
+}
--- LEGO1/mxvideopresenter.h
+++ LEGO1/mxvideopresenter.h
@@ -3,12 +3,17 @@
 
 #include "mxmediapresenter.h"
 
+#include "decomp.h"
+
 class MxVideoPresenter : public MxMediaPresenter
 {
 public:
+  MxVideoPresenter() {
+    Init();
+  }
   // OFFSET: LEGO1 0x1000c820
   inline virtual const char *ClassName() const override // vtable+0x0c
-  { 
+  {
     // 0x100f0760
     return "MxVideoPresenter";
   }
@@ -18,6 +23,15 @@ public:
   {
     return !strcmp(name, MxVideoPresenter::ClassName()) || MxMediaPresenter::IsA(name);
   }
+
+  void Init();
+
+  undefined4 m_unk50;
+  undefined4 m_unk54;
+  undefined4 m_unk58;
+  short m_unk5c;
+  unsigned char m_flags;
+  int m_unk60;
 };
 
 #endif // MXVIDEOPRESENTER_H
