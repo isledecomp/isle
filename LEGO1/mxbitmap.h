793bf42495b4488e52805ae7c738c987a7879aa6
Christian Semmler <mail@csemmler.com>
2023-07-01 13:23:34 +0200

    lego1: match MxBitmap::Read and vtable

--- LEGO1/mxbitmap.h
+++ LEGO1/mxbitmap.h
@@ -11,9 +11,21 @@ class MxBitmap : public MxCore
 {
 public:
   __declspec(dllexport) MxBitmap();
-  __declspec(dllexport) virtual ~MxBitmap();
-  __declspec(dllexport) virtual MxPalette *CreatePalette();
-  __declspec(dllexport) virtual long Read(const char *);
+  __declspec(dllexport) virtual ~MxBitmap(); // vtable+00
+
+  virtual int vtable14(int);
+  virtual int vtable18(BITMAPINFOHEADER *p_bmiHeader);
+  virtual int vtable1c(int p_width, int p_height, MxPalette *p_palette, int);
+  virtual MxResult LoadFile(HANDLE p_handle);
+  __declspec(dllexport) virtual long Read(const char *); // vtable+24
+  virtual int vtable28(int);
+  virtual void vtable2c(int, int, int, int, int, int, int);
+  virtual void vtable30(int, int, int, int, int, int, int);
+  __declspec(dllexport) virtual MxPalette *CreatePalette(); // vtable+34
+  virtual void vtable38(void*);
+  virtual int vtable3c(MxBool);
+  virtual int vtable40(HDC p_hdc, int p_xSrc, int p_ySrc, int p_xDest, int p_yDest, int p_destWidth, int p_destHeight);
+
 private:
   BITMAPINFO *m_info;
   BITMAPINFOHEADER *m_bmiHeader;
