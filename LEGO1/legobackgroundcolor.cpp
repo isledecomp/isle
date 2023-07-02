07e806c4b4edd06cad58da66284408162ae02bfc
Christian Semmler <mail@csemmler.com>
2023-07-02 18:11:29 +0200

    Update legobackgroundcolor.cpp

--- LEGO1/legobackgroundcolor.cpp
+++ LEGO1/legobackgroundcolor.cpp
@@ -4,9 +4,9 @@
 #include "legoutil.h"
 #include "legovideomanager.h"
 
-const char *delimiter = "\t";
-const char *set = "set";
-const char *reset = "reset";
+const char *g_delimiter = "\t";
+const char *g_set = "set";
+const char *g_reset = "reset";
 
 // OFFSET: LEGO1 0x1003bfb0
 LegoBackgroundColor::LegoBackgroundColor(const char *p_name, const char *p_colorString)
@@ -28,23 +28,23 @@ void LegoBackgroundColor::SetColorString(const char *p_colorString)
 
   float converted_r, converted_g, converted_b;
   char *colorStringCopy = strcpy(new char[strlen(p_colorString) + 1], p_colorString);
-  char *colorStringSplit = strtok(colorStringCopy, delimiter);
+  char *colorStringSplit = strtok(colorStringCopy, g_delimiter);
 
-  if (!strcmp(colorStringSplit, set)) {
-    colorStringSplit = strtok(0, delimiter);
+  if (!strcmp(colorStringSplit, g_set)) {
+    colorStringSplit = strtok(0, g_delimiter);
     if (colorStringSplit)
       h = (float) (atoi(colorStringSplit) * 0.01);
-    colorStringSplit = strtok(0, delimiter);
+    colorStringSplit = strtok(0, g_delimiter);
     if (colorStringSplit)
       s = (float) (atoi(colorStringSplit) * 0.01);
-    colorStringSplit = strtok(0, delimiter);
+    colorStringSplit = strtok(0, g_delimiter);
     if (colorStringSplit)
       v = (float) (atoi(colorStringSplit) * 0.01);
 
     ConvertHSVToRGB(this->h, this->s, this->v, &converted_r, &converted_g, &converted_b);
     videomanager->SetSkyColor(converted_r, converted_g, converted_b);
   }
-  else if (!strcmp(colorStringSplit, reset)) {
+  else if (!strcmp(colorStringSplit, g_reset)) {
     ConvertHSVToRGB(this->h, this->s, this->v, &converted_r, &converted_g, &converted_b);
     videomanager->SetSkyColor(converted_r, converted_g, converted_b);
   }
