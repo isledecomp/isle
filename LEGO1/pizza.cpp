598ffdb8e4eb09f042580d78971e93442a4ee2ef
Joshua Peisach <itzswirlz2020@outlook.com>
2023-07-05 19:51:43 -0400

    start pizza

--- LEGO1/pizza.cpp
+++ LEGO1/pizza.cpp
@@ -1,13 +1,18 @@
 #include "pizza.h"
 
-// OFFSET: LEGO1 0x10037ef0 STUB
+// OFFSET: LEGO1 0x10037ef0
 Pizza::Pizza()
 {
-  // TODO
+  this->m_unk80 = 0;
+  this->m_unk84 = 0;
+  this->m_unk88 = 0;
+  this->m_unk8c = -1;
+  this->m_unk98 = 0;
+  this->m_unk90 = 0x80000000;
 }
 
-// OFFSET: LEGO1 0x10038100 STUB
+// OFFSET: LEGO1 0x10038100
 Pizza::~Pizza()
 {
-  // TODO
+  delete this;
 }
--- LEGO1/pizza.h
+++ LEGO1/pizza.h
@@ -1,7 +1,9 @@
 #ifndef PIZZA_H
 #define PIZZA_H
 
+#include "decomp.h"
 #include "isleactor.h"
+#include "mxtypes.h"
 
 // VTABLE 0x100d7380
 // SIZE 0x9c
@@ -10,7 +12,13 @@ class Pizza : public IsleActor
 public:
   Pizza();
   virtual ~Pizza() override;
-
+private:
+  undefined4 m_unk80;
+  undefined4 m_unk84;
+  undefined4 m_unk88;
+  undefined4 m_unk8c;
+  undefined4 m_unk90;
+  undefined4 m_unk98;
 };
 
 #endif // PIZZA_H
