73ad680ed23f12e20c5a952b4815fa1791b2e2bd
Brendan Dougherty <brandougherty1@gmail.com>
2023-07-10 12:27:37 -0500

    Add example of template instantiation comparison.

--- LEGO1/mxnotificationmanager.cpp
+++ LEGO1/mxnotificationmanager.cpp
@@ -1,5 +1,16 @@
 #include "mxnotificationmanager.h"
 
+// OFFSET: LEGO1 0x100ac320 TEMPLATE
+// list<unsigned int,allocator<unsigned int> >::~list<unsigned int,allocator<unsigned int> >
+
+// FIXME: Example of template compare functionality, remove before merging.
+#include <stl.h>
+#include <iostream>
+void make_a_list() {
+  List<unsigned int> l;
+  cout << l.size();
+}
+
 // OFFSET: LEGO1 0x100ac450 STUB
 MxNotificationManager::~MxNotificationManager()
 {
