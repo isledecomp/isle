d49946c534e0b4205f90696403e8fe8a4b0f1aea
disinvite <disinvite@users.noreply.github.com>
2023-07-16 18:19:22 -0400

    Fixing up MXIOINFO to prepare for merge.
    * Following feedback from @stravant and @itsmattkc, reverted back to using `MMIOINFO` struct as the class member instead of adding its values directly. (We actually gained a little on accuracy with this change.)
    * The memset to zero out the values in the constructor now acts on `m_info` instead of `this`. Strictly speaking we don't need the size assert any more but I decided to keep it in case we change the members later for some reason.
    * Casting the `hmmio` member to `HFILE` (int) or `HMMIO` (WORD) typedefs where needed.
    * Squelch a signed/unsigned type comparison warning

--- LEGO1/mxioinfo.h
+++ LEGO1/mxioinfo.h
@@ -21,31 +21,9 @@ public:
   MxU16 Advance(MxU16);
   MxU16 Descend(MMCKINFO *, const MMCKINFO *, MxU16);
 
-  // The following is the MMIOINFO struct but with h_mmio set to type HFILE.
-
-  /* general fields */
-  DWORD           m_dwFlags;        /*  0 general status flags */
-  FOURCC          m_fccIOProc;      /*  4 pointer to I/O procedure */
-  LPMMIOPROC      m_pIOProc;        /*  8 pointer to I/O procedure */
-  UINT            m_wErrorRet;      /*  c place for error to be returned */
-  HTASK           m_htask;          /* 10 alternate local task */
-
-  /* fields maintained by MMIO functions during buffered I/O */
-  LONG            m_cchBuffer;      /* 14 size of I/O buffer (or 0L) */
-  HPSTR           m_pchBuffer;      /* 18 start of I/O buffer (or NULL) */
-  HPSTR           m_pchNext;        /* 1c pointer to next byte to read/write */
-  HPSTR           m_pchEndRead;     /* 20 pointer to last valid byte to read */
-  HPSTR           m_pchEndWrite;    /* 24 pointer to last byte to write */
-  LONG            m_lBufOffset;     /* 28 disk offset of start of buffer */
-
-  /* fields maintained by I/O procedure */
-  LONG            m_lDiskOffset;    /* 2c disk offset of next read or write */
-  DWORD           m_adwInfo[3];     /* 30 data specific to type of MMIOPROC */
-
-  /* other fields maintained by MMIO */
-  DWORD           m_dwReserved1;    /* 3c reserved for MMIO use */
-  DWORD           m_dwReserved2;    /* 40 reserved for MMIO use */
-  HFILE           m_hmmio;          /* 44 handle to open file */
+  // NOTE: In MXIOINFO, the `hmmio` member of MMIOINFO is used like
+  // an HFILE (int) instead of an HMMIO (WORD).
+  MMIOINFO m_info;
 };
 
 #endif // MXIOINFO_H
