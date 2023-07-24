7df05beaf82e128be0b6a793ec32d7a9fd1b2dfc
Joshua Peisach <itzswirlz2020@outlook.com>
2023-07-24 13:26:10 -0400

    Adjust variables

--- LEGO1/islepathactor.cpp
+++ LEGO1/islepathactor.cpp
@@ -4,8 +4,8 @@
 IslePathActor::IslePathActor()
 {
   this->m_pLegoWorld = NULL;
-  this->m_unk13c = 0x40c00000;
-  this->m_fourcc = 0x3f800000;
+  this->m_unk13c = 6.0;
+  this->m_unk15c = 1.0;
   this->m_unk158 = 0;
 }
 
