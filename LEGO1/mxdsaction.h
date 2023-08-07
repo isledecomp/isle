9a688ec65dae1b34160dbcb19e2140fdeca4de9b
Christian Semmler <mail@csemmler.com>
2023-08-07 11:52:49 -0400

    Implement most of MxDSAction

--- LEGO1/mxdsaction.h
+++ LEGO1/mxdsaction.h
@@ -13,6 +13,9 @@ public:
   __declspec(dllexport) MxDSAction();
   __declspec(dllexport) virtual ~MxDSAction();
 
+  void CopyFrom(MxDSAction &p_dsAction);
+  MxDSAction &operator=(MxDSAction &p_dsAction);
+
   // OFFSET: LEGO1 0x100ad980
   inline virtual const char *ClassName() const override // vtable+0x0c
   {
@@ -25,21 +28,35 @@ public:
   {
     return !strcmp(name, MxDSAction::ClassName()) || MxDSObject::IsA(name);
   }
+
+  virtual MxU32 GetSizeOnDisk(); // vtable+18;
+  virtual void Deserialize(char **p_source, MxS16 p_unk24); // vtable+1c;
+  virtual MxLong GetDuration(); // vtable+24;
+  virtual void SetDuration(LONG p_duration); // vtable+28;
+  virtual MxDSAction *Clone(); // vtable+2c;
+  virtual void MergeFrom(MxDSAction &p_dsAction); // vtable+30;
+  virtual MxBool HasId(MxU32 p_objectId); // vtable+34;
+  virtual void SetSomeTimingField(MxLong p_someTimingField); // vtable+38;
+  virtual MxLong GetSomeTimingField(); // vtable+3c;
+  virtual MxLong GetCurrentTime(); // vtable+40;
+
+  void FUN_100ADE60(MxU16 p_length, void *p_data);
+
 private:
-  undefined4 m_unk2c;
+  undefined4 m_sizeOnDisk;
   DWORD m_flags;
   DWORD m_startTime;
-  LONG m_duration;
+  MxLong m_duration;
   MxS32 m_loopCount;
   MxVector3Data m_location;
   MxVector3Data m_direction;
   MxVector3Data m_up;
   undefined4 *m_unk7c;
-  undefined2 m_unk80;
+  MxU16 m_unk80;
   undefined4 m_unk84;
   undefined4 m_unk88;
   MxOmni* m_omni; // 0x8c
-  MxS32 m_someTimingField; // 0x90
+  MxLong m_someTimingField; // 0x90
 };
 
 #endif // MXDSACTION_H
