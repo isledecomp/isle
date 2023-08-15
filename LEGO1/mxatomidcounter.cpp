7c3e87093238299fb0031538043b5087e6ef77e7
disinvite <disinvite@users.noreply.github.com>
2023-08-15 18:34:24 -0400

    Add some size asserts

--- LEGO1/mxatomidcounter.cpp
+++ LEGO1/mxatomidcounter.cpp
@@ -1,4 +1,8 @@
 #include "mxatomidcounter.h"
+#include "decomp.h"
+
+DECOMP_SIZE_ASSERT(MxAtomIdCounter, 0x14);
+DECOMP_SIZE_ASSERT(MxAtomIdCounterSet, 0x10);
 
 // OFFSET: LEGO1 0x100ad7f0
 void MxAtomIdCounter::Inc()
