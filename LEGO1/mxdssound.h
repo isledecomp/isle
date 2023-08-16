51ac526f8accb38859cddc4a9836710492cbd304
Christian Semmler <mail@csemmler.com>
2023-08-16 13:15:59 -0400

    Add MxDSSound (#111)

--- LEGO1/mxdssound.h
+++ LEGO1/mxdssound.h
@@ -2,7 +2,6 @@
 #define MXDSSOUND_H
 
 #include "mxdsmediaaction.h"
-#include "mxtypes.h"
 
 // VTABLE 0x100dcdd0
 // SIZE 0xc0
@@ -11,6 +10,9 @@ class MxDSSound : public MxDSMediaAction
 public:
   MxDSSound();
   virtual ~MxDSSound() override;
+
+  void CopyFrom(MxDSSound &p_dsSound);
+  MxDSSound &operator=(MxDSSound &p_dsSound);
 
   // OFFSET: LEGO1 0x100c9330
   inline virtual const char *ClassName() const override // vtable+0x0c
@@ -24,10 +26,14 @@ public:
   {
     return !strcmp(name, MxDSSound::ClassName()) || MxDSMediaAction::IsA(name);
   }
-private:
-  MxS32 m_unkb8;
-  MxLong m_lastField; // 0xbc
-};
 
+  virtual MxU32 GetSizeOnDisk(); // vtable+18;
+  virtual void Deserialize(char **p_source, MxS16 p_unk24); // vtable+1c;
+  virtual MxDSAction *Clone(); // vtable+2c;
+
+private:
+  MxU32 m_sizeOnDisk;
+  MxS32 m_volume; // 0xbc
+};
 
 #endif // MXDSSOUND_H
