28f961b86d0c130bd46607b690731dfc42faf238
Christian Semmler <mail@csemmler.com>
2023-08-16 19:38:26 -0400

    Add MxDSAnim, MxDSEvent, MxDSObjectAction, MxDSStill (#112)
    * Add MxDSAnim
    
    * Add MxDSEvent, MxDSObjectAction, MxDSStill

--- LEGO1/mxdsstill.cpp
+++ LEGO1/mxdsstill.cpp
@@ -1,5 +1,7 @@
 #include "mxdsstill.h"
 
+DECOMP_SIZE_ASSERT(MxDSStill, 0xb8)
+
 // OFFSET: LEGO1 0x100c98c0
 MxDSStill::MxDSStill()
 {
@@ -9,4 +11,31 @@ MxDSStill::MxDSStill()
 // OFFSET: LEGO1 0x100c9a70
 MxDSStill::~MxDSStill()
 {
+}
+
+// OFFSET: LEGO1 0x100c9ac0
+void MxDSStill::CopyFrom(MxDSStill &p_dsStill)
+{
+}
+
+// OFFSET: LEGO1 0x100c9ad0
+MxDSStill &MxDSStill::operator=(MxDSStill &p_dsStill)
+{
+  if (this == &p_dsStill)
+    return *this;
+
+  MxDSMediaAction::operator=(p_dsStill);
+  this->CopyFrom(p_dsStill);
+  return *this;
+}
+
+// OFFSET: LEGO1 0x100c9b00
+MxDSAction *MxDSStill::Clone()
+{
+  MxDSStill *clone = new MxDSStill();
+
+  if (clone)
+    *clone = *this;
+
+  return clone;
 }
