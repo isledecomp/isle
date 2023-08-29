004d28240e36cd534d3cf00f5fa997a05743fef2
Misha <mishakeys20@gmail.com>
2023-08-29 18:54:19 -0400

    Match SetUserMaxLOD and implement UpdateMaxLOD

--- LEGO1/realtimeview.cpp
+++ LEGO1/realtimeview.cpp
@@ -1,10 +1,18 @@
 #include "realtimeview.h"
+#include <math.h>
+
+// 0x10109598
+float g_userMaxLodPower;
+
+// 0x10101044
+float g_userMaxBase = 4.0f;
 
 // 0x10101048
 float g_userMaxLod = 3.6f;
 
 // 0x1010104c
 float g_partsThreshold = 1000.0f;
+
 
 // OFFSET: LEGO1 0x100a5e00
 float RealtimeView::GetUserMaxLOD()
@@ -19,10 +27,17 @@ float RealtimeView::GetPartsThreshold()
   return g_partsThreshold;
 }
 
+// OFFSET: LEGO1 100a5e20
+void RealtimeView::UpdateMaxLOD()
+{
+  g_userMaxLodPower = pow(g_userMaxBase, -g_userMaxLod);
+}
+
 // OFFSET: LEGO1 0x100a5de0
 void RealtimeView::SetUserMaxLOD(float p_lod)
 {
   g_userMaxLod = p_lod;
+  UpdateMaxLOD();
 }
 
 // OFFSET: LEGO1 0x100a5df0
