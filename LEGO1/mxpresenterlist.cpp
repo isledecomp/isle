e1e2abc5107c1754d7b6a52dba9a2a84b3b3de08
Christian Semmler <mail@csemmler.com>
2023-09-25 15:53:57 -0400

    Implement/match MxDSSerialAction (#139)
    * Implement/match MxDSSerialAction
    
    * Add neccessary MxDSMultiAction functions

--- LEGO1/mxpresenterlist.cpp
+++ LEGO1/mxpresenterlist.cpp
@@ -2,6 +2,7 @@
 #include "mxpresenter.h"
 
 DECOMP_SIZE_ASSERT(MxPresenterList, 0x18);
+DECOMP_SIZE_ASSERT(MxPresenterListCursor, 0x10);
 
 // OFFSET: LEGO1 0x1001cd00
 MxS8 MxPresenterList::Compare(MxPresenter *p_var0, MxPresenter *p_var1)
