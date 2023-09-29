5b7954a97bb6c20cb03f1cd5b114c2001da8113c
Joshua Peisach <itzswirlz2020@outlook.com>
2023-09-29 12:51:05 -0400

    MxMusicPresenter: ClassName, IsA, ctor, Init (#145)

--- LEGO1/mxmusicpresenter.h
+++ LEGO1/mxmusicpresenter.h
@@ -7,7 +7,23 @@
 class MxMusicPresenter : public MxAudioPresenter
 {
 public:
+  // OFFSET: LEGO1 0x100c23a0
+  inline virtual const char *ClassName() const override // vtable+0xc
+  {
+    // 0x10101e48
+    return "MxMusicPresenter";
+  }
+
+  // OFFSET: LEGO1 0x100c23b0
+  inline virtual MxBool IsA(const char *name) const override // vtable+0x10
+  {
+    return !strcmp(name, MxMusicPresenter::ClassName()) || MxAudioPresenter::IsA(name);
+  }
+
   MxMusicPresenter();
+
+private:
+  void Init();
 };
 
 #endif // MXMUSICPRESENTER_H
