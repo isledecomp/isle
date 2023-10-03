dd1e30fb7260d316121119680351a7993c46b21c
Joshua Peisach <itzswirlz2020@outlook.com>
2023-10-03 17:01:26 -0400

    MxLoopingFlcPresenter - finish Init, stub Destroy function, match dtor

--- LEGO1/mxloopingflcpresenter.cpp
+++ LEGO1/mxloopingflcpresenter.cpp
@@ -13,11 +13,19 @@ MxLoopingFlcPresenter::MxLoopingFlcPresenter()
 // OFFSET: LEGO1 0x100b43b0 STUB
 MxLoopingFlcPresenter::~MxLoopingFlcPresenter()
 {
-  // TODO
+  Destroy(true);
 }
 
 // OFFSET: LEGO1 0x100b4410
 void MxLoopingFlcPresenter::Init()
 {
   this->m_unk68 = 0;
+  this->m_flags &= 0xfd;
+  this->m_flags &= 0xfb;
+}
+
+// OFFSET: LEGO1 0x100b4432 STUB
+void MxLoopingFlcPresenter::Destroy(MxBool p_param)
+{
+  // TODO 
 }
