9fc9c9e817000f63ad9b9dea3b1d480cf2f3277f
Joshua Peisach <itzswirlz2020@outlook.com>
2023-10-04 13:25:40 -0400

    MxLoopingSmkPresenter - dtor, Init, stub Destroy

--- LEGO1/mxloopingsmkpresenter.cpp
+++ LEGO1/mxloopingsmkpresenter.cpp
@@ -10,14 +10,22 @@ MxLoopingSmkPresenter::MxLoopingSmkPresenter()
   Init();
 }
 
-// OFFSET: LEGO1 0x100b4950 STUB
+// OFFSET: LEGO1 0x100b4950
 MxLoopingSmkPresenter::~MxLoopingSmkPresenter()
 {
-  // TODO
+  Destroy(TRUE);
 }
 
-// OFFSET: LEGO1 0x100b49b0 STUB
+// OFFSET: LEGO1 0x100b49b0
 void MxLoopingSmkPresenter::Init()
 {
-  // TODO
+  this->m_unk720 = 0;
+  this->m_flags &= 0xfd;
+  this->m_flags &= 0xfb;
+}
+
+// OFFSET: LEGO1 0x100b49d0 STUB
+void MxLoopingSmkPresenter::Destroy(MxBool p_bool)
+{
+  // TODO - theres a chain of destroy and free function calls here (FUN_100b4300 -> FUN_100b3900 -> FUN_100c5d40 -> function at 0x100b27b0)
 }
