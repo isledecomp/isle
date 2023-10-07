5957674e819a9cb873343a1508cacef8a52b8835
Joshua Peisach <itzswirlz2020@outlook.com>
2023-10-07 15:47:19 -0400

    MxMusicPresenter vtable
    vtable+0x34 adds the current presenter to the music manager
    vtable+0x38 calls destroy with the param as "FALSE"

--- LEGO1/mxmusicpresenter.cpp
+++ LEGO1/mxmusicpresenter.cpp
@@ -1,5 +1,11 @@
 #include "mxmusicpresenter.h"
 
+#include "decomp.h"
+#include "mxmusicmanager.h"
+#include "mxomni.h"
+
+DECOMP_SIZE_ASSERT(MxMusicPresenter, 0x54);
+
 // OFFSET: LEGO1 0x100c22c0
 MxMusicPresenter::MxMusicPresenter()
 {
@@ -21,4 +27,22 @@ void MxMusicPresenter::Init()
 void MxMusicPresenter::Destroy(MxBool)
 {
   // TODO
+}
+
+// OFFSET: LEGO1 0x100c25a0
+MxResult MxMusicPresenter::AddToMusicManager()
+{
+  MxResult ret = -1;
+  if(MusicManager()) {
+    ret = 0;
+    MusicManager()->AddPresenter(*this);
+  }
+  return ret;
+} 
+
+// OFFSET: LEGO1 0x100c25d0
+void MxMusicPresenter::vtable38()
+{
+  // TODO: Name this function when we know what the argument to Destroy does
+  Destroy(FALSE);
 }
