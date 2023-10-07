c152473605f89bbc2b25a2361167ca19c9706a5d
Joshua Peisach <itzswirlz2020@outlook.com>
2023-10-07 17:20:27 -0400

    Start MxCompositePresenter ctor/dtor

--- LEGO1/mxcompositepresenter.cpp
+++ LEGO1/mxcompositepresenter.cpp
@@ -1,17 +1,21 @@
 #include "mxcompositepresenter.h"
 
 #include "decomp.h"
+#include "mxnotificationmanager.h"
 
 DECOMP_SIZE_ASSERT(MxCompositePresenter, 0x4c);
 
-// OFFSET: LEGO1 0x100b60b0 STUB
+// OFFSET: LEGO1 0x100b60b0
 MxCompositePresenter::MxCompositePresenter()
 {
-  // TODO
+  this->m_unk44 = (undefined4*) malloc(0xc) + 3;
+  this->m_unk40 = 0;
+  this->m_unk48 = 0;
+  NotificationManager()->Register(this);
 }
 
-// OFFSET: LEGO1 0x100b6390 STUB
+// OFFSET: LEGO1 0x100b6390
 MxCompositePresenter::~MxCompositePresenter()
 {
-  // TODO
+  NotificationManager()->Unregister(this);
 }
