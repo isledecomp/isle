6b2dc7a6fb2e7ec7eaf1a7e1c43ee2bfabf01115
ChrisMiuchiz <chris000005@gmail.com>
2023-10-07 23:52:50 -0400

    Remove goto from isleapp.cpp

--- ISLE/isleapp.cpp
+++ ISLE/isleapp.cpp
@@ -251,15 +251,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
       }
 
       if (g_mousedown == 0) {
-LAB_00401bc7:
-        if (g_mousemoved) {
-          g_mousemoved = FALSE;
-        }
-      } else if (g_mousemoved) {
-        if (g_isle) {
+        // Nothing
+      }
+      else if (g_mousemoved && g_isle) {
           g_isle->Tick(0);
-        }
-        goto LAB_00401bc7;
+      }
+
+      if (g_mousemoved) {
+        g_mousemoved = FALSE;
       }
     }
   }
