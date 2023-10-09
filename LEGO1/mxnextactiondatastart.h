8500407bab33fac0ce237ed8029eec1f491802ee
Misha <mishakeys20@gmail.com>
2023-10-09 19:52:13 -0400

    Revert "MxNextActionDataStart"
    This reverts commit 41e8cfa2f11ea4f9b39df0bdf0c3657fdbd9efe9.

--- LEGO1/mxnextactiondatastart.h
+++ LEGO1/mxnextactiondatastart.h
@@ -6,22 +6,7 @@
 // VTABLE 0x100dc9a0
 class MxNextActionDataStart : public MxCore
 {
-  // OFFSET: LEGO1 0x100c1990
-  virtual ~MxNextActionDataStart() override;
-  {
-
-  }
-  inline virtual const char *ClassName() const override // vtable+0x0c
-  {
-    // 0x100c1900
-    return "MxNextActionDataStart";
-  }
-
-  // OFFSET: LEGO1 0x100c1910
-  inline virtual MxBool IsA(const char *p_name) const override // vtable+0x10
-  {
-    return !strcmp(p_name, MxNextActionDataStart::ClassName()) || MxCore::IsA(p_name);
-  }
+  
 };
 
 #endif // MXNEXTACTIONDATASTART_H
