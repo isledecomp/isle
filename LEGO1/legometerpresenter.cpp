7cf85e6f063d7fa03951abec1cce91f6cca2605f
Andrew Gutekanst <andrew.gutekanst@gmail.com>
2023-10-14 09:08:51 -0400

    Add LegoMeterPresenter stub class (#206)

---
+++ LEGO1/legometerpresenter.cpp
@@ -0,0 +1,6 @@
+#include "legometerpresenter.h"
+
+#include "decomp.h"
+
+// Uncomment when member class variables are fleshed out.
+// DECOMP_SIZE_ASSERT(LegoMeterPresenter, 0x94); // 0x1000a163
---
+++ LEGO1/legometerpresenter.h
@@ -0,0 +1,14 @@
+#ifndef LEGOMETERPRESENTER_H
+#define LEGOMETERPRESENTER_H
+
+#include "mxstillpresenter.h"
+
+// VTABLE 0x100d7ac8
+// SIZE 0x94 (from 0x1000a163)
+class LegoMeterPresenter : public MxStillPresenter
+{
+public:
+  // MxStillPresenter's `::ClassName` and `::IsA` are used.
+};
+
+#endif // LEGOMETERPRESENTER_H
