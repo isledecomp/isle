1e3c346aa18eee71a82d9ddfa21f69933a1db871
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2023-10-16 16:09:24 -0400

    complete and match LegoState vtable

--- LEGO1/legostate.cpp
+++ LEGO1/legostate.cpp
@@ -11,3 +11,23 @@ LegoState::~LegoState()
 MxBool LegoState::VTable0x14() {
   return TRUE;
 }
+
+// OFFSET: LEGO1 0x10005fa0
+MxBool LegoState::VTable0x18() {
+  return FALSE;
+}
+
+// OFFSET: LEGO1 0x10005fb0
+MxResult LegoState::VTable0x1C(LegoState *p_legoState)
+{
+  if (p_legoState->VTable0x14()) {
+    p_legoState->FUN_10006030(this->ClassName());
+  }
+  return SUCCESS;
+}
+
+// OFFSET: LEGO1 0x10006030 STUB
+void LegoState::FUN_10006030(MxString p_str)
+{
+  // TODO
+}
