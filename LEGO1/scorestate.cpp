f4b006a8af0a9ddbf8da237cc8ba4b8a547dbe79
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2023-10-18 21:58:16 -0400

    implement/match ScoreState vtable

--- LEGO1/scorestate.cpp
+++ LEGO1/scorestate.cpp
@@ -1,1 +1,13 @@
 #include "scorestate.h"
+
+// OFFSET: LEGO1 0x1000de20
+MxBool ScoreState::VTable0x14() {
+  return FALSE;
+}
+
+// OFFSET: LEGO1 0x1000de30
+MxBool ScoreState::VTable0x18()
+{
+  m_unk0x08 = TRUE;
+  return TRUE;
+}
