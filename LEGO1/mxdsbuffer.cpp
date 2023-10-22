724c9539473214e975f228cf133e5ef458398b9d
Misha <106913236+MishaProductions@users.noreply.github.com>
2023-10-22 15:58:05 -0400

    Implement MxDSBuffer constructor, match MxDSSource::FUN_100bffd0 (#235)
    * Implement MxDSBuffer constructor, match FUN_100bffd0
    
    * Match to 100%, minor style fixes
    
    ---------
    
    Co-authored-by: Christian Semmler <mail@csemmler.com>

--- LEGO1/mxdsbuffer.cpp
+++ LEGO1/mxdsbuffer.cpp
@@ -1,13 +1,37 @@
 #include "mxdsbuffer.h"
 
+DECOMP_SIZE_ASSERT(MxDSBuffer, 0x34);
+
 // OFFSET: LEGO1 0x100c6470
 MxDSBuffer::MxDSBuffer()
 {
-  // TODO
+  m_unk20 = 0;
+  m_pBuffer = NULL;
+  m_pIntoBuffer = NULL;
+  m_pIntoBuffer2 = NULL;
+  m_unk14 = 0;
+  m_unk18 = 0;
+  m_unk1c = 0;
+  m_writeOffset = 0;
+  m_bytesRemaining = 0;
+  m_mode = 2;
+  m_unk30 = 0;
 }
 
 // OFFSET: LEGO1 0x100c6530
 MxDSBuffer::~MxDSBuffer()
 {
   // TODO
+}
+
+// OFFSET: LEGO1 0x100c6780
+MxResult MxDSBuffer::FUN_100c6780(void* p_buffer, MxU32 p_size)
+{
+  m_pBuffer = p_buffer;
+  m_pIntoBuffer = p_buffer;
+  m_pIntoBuffer2 = p_buffer;
+  m_bytesRemaining = p_size;
+  m_writeOffset = p_size;
+  m_mode = 2;
+  return SUCCESS;
 }
