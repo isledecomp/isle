cb55b6c3558cc4d8b575d98290fd5b9c3178fb66
vMidz <147055059+vMidz@users.noreply.github.com>
2023-10-23 16:48:55 +0300

    implement LegoPathPresenter
    implement some LegoPathPresenter functions

--- LEGO1/legopathpresenter.cpp
+++ LEGO1/legopathpresenter.cpp
@@ -1,7 +1,68 @@
 #include "legopathpresenter.h"
+#include "legoomni.h"
+#include "legovideomanager.h"
+#include "mxautolocker.h"
 
-// OFFSET: LEGO1 0x100448d0 STUB
+DECOMP_SIZE_ASSERT(LegoPathPresenter, 0x54);
+
+// OFFSET: LEGO1 0x10044ab0
+void LegoPathPresenter::Init() {}
+
+// OFFSET: LEGO1 0x100448d0
 LegoPathPresenter::LegoPathPresenter()
 {
+  const char *internal = this->m_atomId.GetInternal();
+  internal = NULL;
+  Init();
+}
+
+// OFFSET: LEGO1 0x10044d40
+void LegoPathPresenter::RepeatingTickle()
+{
+  if (this->m_action->GetDuration() == -1)
+  {
+    return;
+  }
+  EndAction();
+}
+
+// OFFSET: LEGO1 0x10044d60 STUB
+void LegoPathPresenter::ParseExtra()
+{
   // TODO
+}
+
+// OFFSET: LEGO1 0x10044b40
+MxResult LegoPathPresenter::AddToManager()
+{
+  MxResult status = FAILURE;
+  if (VideoManager())
+  {
+    VideoManager()->AddPresenter(*this);
+    status = SUCCESS;
+  }
+  return status;
+}
+
+// OFFSET: LEGO1 0x10044b70
+void LegoPathPresenter::Destroy(MxBool p_fromDestructor)
+{
+  if (VideoManager())
+  {
+    VideoManager()->RemovePresenter(*this);
+  }
+  {
+    MxAutoLocker lock(&this->m_criticalSection);
+    Init();
+  }
+  if (!p_fromDestructor)
+  {
+    MxMediaPresenter::Destroy(0);
+  }
+}
+
+// OFFSET: LEGO1 0x10044c10
+void LegoPathPresenter::Destroy()
+{
+  Destroy(FALSE);
 }
