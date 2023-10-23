7206d63e17f7c458e3c9b9d55c237654a7b2f64d
Joshua Peisach <itzswirlz2020@outlook.com>
2023-10-23 09:27:22 -0400

    MxWavePresenter ClassName, IsA

--- LEGO1/mxwavepresenter.h
+++ LEGO1/mxwavepresenter.h
@@ -16,6 +16,20 @@ public:
   MxWavePresenter() {
     Init();
   }
+
+  // OFFSET: LEGO1 0x1000d6c0
+  inline virtual const char *ClassName() const override // vtable+0x0c
+  {
+    // 0x100f07b4
+    return "MxWavePresenter";
+  }
+
+  // OFFSET: LEGO1 0x1000d6d0
+  inline virtual MxBool IsA(const char *name) const override // vtable+0x10
+  {
+    return !strcmp(name, MxWavePresenter::ClassName()) || MxSoundPresenter::IsA(name);
+  }
+
   undefined4 m_unk54;
   undefined4 m_unk58;
   undefined4 m_unk5c;
