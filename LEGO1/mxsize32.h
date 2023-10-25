a2a5cd1186c11ce3704db62343e7be43103cf986
Christian Semmler <mail@csemmler.com>
2023-10-25 06:38:36 -0400

    Format

--- LEGO1/mxsize32.h
+++ LEGO1/mxsize32.h
@@ -3,18 +3,17 @@
 
 #include "mxtypes.h"
 
-class MxSize32
-{
+class MxSize32 {
 public:
-  MxSize32() { }
-  MxSize32(MxS32 p_width, MxS32 p_height)
-  {
-    this->m_width = p_width;
-    this->m_height = p_height;
-  }
+	MxSize32() {}
+	MxSize32(MxS32 p_width, MxS32 p_height)
+	{
+		this->m_width = p_width;
+		this->m_height = p_height;
+	}
 
-  MxS32 m_width;
-  MxS32 m_height;
+	MxS32 m_width;
+	MxS32 m_height;
 };
 
 #endif // MXSIZE32_H
