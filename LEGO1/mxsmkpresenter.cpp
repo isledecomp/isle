153f4a872e94613f937677a15d20ec2ba4ab2550
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2023-10-27 13:00:20 -0400

    implement/match several mxsmkpresenter functions (#251)
    * implement/match several mxsmkpresenter functions
    
    * formatting
    
    * formatting (sequel)
    
    * Match remaining functions
    
    ---------
    
    Co-authored-by: Christian Semmler <mail@csemmler.com>

--- LEGO1/mxsmkpresenter.cpp
+++ LEGO1/mxsmkpresenter.cpp
@@ -4,16 +4,40 @@
 
 DECOMP_SIZE_ASSERT(MxSmkPresenter, 0x720);
 
-// OFFSET: LEGO1 0x100b3650 STUB
+// OFFSET: LEGO1 0x100b3650
 MxSmkPresenter::MxSmkPresenter()
 {
-	// TODO
+	Init();
 }
 
-// OFFSET: LEGO1 0x100b38d0 STUB
+// OFFSET: LEGO1 0x100b3870
+MxSmkPresenter::~MxSmkPresenter()
+{
+	Destroy(TRUE);
+}
+
+// OFFSET: LEGO1 0x100b38d0
 void MxSmkPresenter::Init()
 {
-	// TODO
+	m_unk0x71c = 0;
+	memset(&m_mxSmack, 0, sizeof(m_mxSmack));
+	m_flags &= 0xfd;
+	m_flags &= 0xfb;
+}
+
+// OFFSET: LEGO1 0x100b3900
+void MxSmkPresenter::Destroy(MxBool p_fromDestructor)
+{
+	m_criticalSection.Enter();
+
+	FUN_100c5d40(&m_mxSmack);
+	Init();
+
+	m_criticalSection.Leave();
+
+	if (!p_fromDestructor) {
+		MxVideoPresenter::Destroy(FALSE);
+	}
 }
 
 // OFFSET: LEGO1 0x100b3960
@@ -24,5 +48,20 @@ void MxSmkPresenter::VTable0x60()
 	}
 
 	m_bitmap = new MxBitmap();
-	m_bitmap->SetSize(m_smkWidth, m_smkHeight, NULL, FALSE);
+	m_bitmap->SetSize(m_mxSmack.m_smack.m_width, m_mxSmack.m_smack.m_height, NULL, FALSE);
+}
+
+// OFFSET: LEGO1 0x100c5d40
+void MxSmkPresenter::FUN_100c5d40(MxSmack* p_mxSmack)
+{
+	if (p_mxSmack->m_unk0x6a0)
+		delete p_mxSmack->m_unk0x6a0;
+	if (p_mxSmack->m_unk0x6a4)
+		delete p_mxSmack->m_unk0x6a4;
+	if (p_mxSmack->m_unk0x6a8)
+		delete p_mxSmack->m_unk0x6a8;
+	if (p_mxSmack->m_unk0x6ac)
+		delete p_mxSmack->m_unk0x6ac;
+	if (p_mxSmack->m_unk0x6b4)
+		delete p_mxSmack->m_unk0x6b4;
 }
--- LEGO1/mxsmkpresenter.h
+++ LEGO1/mxsmkpresenter.h
@@ -4,22 +4,41 @@
 #include "decomp.h"
 #include "mxvideopresenter.h"
 
+#include <smk.h>
+
 // VTABLE 0x100dc348
 // SIZE 0x720
 class MxSmkPresenter : public MxVideoPresenter {
 public:
 	MxSmkPresenter();
+	virtual ~MxSmkPresenter() override;
 
 	virtual void VTable0x60() override;
 
-	undefined4 m_unk64;
-	MxS32 m_smkWidth;  // 0x68
-	MxS32 m_smkHeight; // 0x6c
-	undefined4 m_unk70[427];
-	undefined4 m_unk71c;
+	struct MxSmack {
+		Smack m_smack;
+
+		// Unknown for the time being. Not an immediately
+		// recognizable part of the SMK standard...
+
+		undefined m_unk0x3f4[784];
+		undefined4* m_unk0x6a0;
+		undefined4* m_unk0x6a4;
+		undefined4* m_unk0x6a8;
+		undefined4* m_unk0x6ac;
+		undefined4* m_unk0x6b0;
+		undefined4* m_unk0x6b4;
+	};
+
+	MxSmack m_mxSmack;
+	undefined4 m_unk0x71c;
 
 private:
 	void Init();
+	void Destroy(MxBool p_fromDestructor);
+
+	// This should most likely be in a separate translation unit
+	static void FUN_100c5d40(MxSmack* p_mxSmack);
 };
 
 #endif // MXSMKPRESENTER_H
