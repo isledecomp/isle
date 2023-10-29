f4646a70750295d20301efb07bf81e9a9476d65b
Misha <106913236+MishaProductions@users.noreply.github.com>
2023-10-29 10:01:14 -0400

    implement a few mxstream* methods (#252)
    * commit code
    
    * commit code
    
    * Update mxdiskstreamprovider.cpp
    
    * Update mxstreamprovider.h
    
    * improve match + add html file to gitignore
    
    * improve match of MxRAMStreamController::Open
    
    * MxDiskStreamController::Open
    
    * Match some functions, relocate m_target to `MxThread`
    
    ---------
    
    Co-authored-by: Christian Semmler <mail@csemmler.com>

--- LEGO1/mxdsfile.h
+++ LEGO1/mxdsfile.h
@@ -31,6 +31,9 @@ public:
 	__declspec(dllexport) virtual MxLong Seek(MxLong, int);               // vtable+0x24
 	__declspec(dllexport) virtual MxULong GetBufferSize();                // vtable+0x28
 	__declspec(dllexport) virtual MxULong GetStreamBuffersNum();          // vtable+0x2c
+
+	inline void SetFileName(const char* p_filename) { m_filename = p_filename; }
+
 private:
 	MxLong ReadChunks();
 	struct ChunkHeader {
