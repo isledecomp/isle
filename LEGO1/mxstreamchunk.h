fc8c68543bb5136004409434f87eff5279435617
Joshua Peisach <itzswirlz2020@outlook.com>
2023-11-03 20:17:23 -0400

    Add missing ClassName's and IsA's
    Exceptions: LegoAnimActor as it does something weird with a call to another private/inlined function(?) and RaceStandsEntity which seems to have two vtables

--- LEGO1/mxstreamchunk.h
+++ LEGO1/mxstreamchunk.h
@@ -4,6 +4,19 @@
 #include "mxdschunk.h"
 
 // VTABLE 0x100dc2a8
-class MxStreamChunk : public MxDSChunk {};
+class MxStreamChunk : public MxDSChunk {
+    // OFFSET: LEGO1 0x100b1fe0
+	inline virtual const char* ClassName() const override // vtable+0xc
+	{
+		// 0x10101e5c
+		return "MxStreamChunk";
+	}
+
+	// OFFSET: LEGO1 0x100b1ff0
+	inline virtual MxBool IsA(const char* name) const override // vtable+0x10
+	{
+		return !strcmp(name, MxStreamChunk::ClassName()) || MxDSChunk::IsA(name);
+	}
+};
 
 #endif // MXSTREAMCHUNK_H
--- LEGO1/radio.h
+++ LEGO1/radio.h
@@ -7,6 +7,19 @@
 class Radio : public MxCore {
 public:
 	virtual ~Radio() override;
+
+	// OFFSET: LEGO1 0x1002c8e0
+	inline virtual const char* ClassName() const override // vtable+0x0c
+	{
+		// 0x100f328c
+		return "Radio";
+	}
+
+	// OFFSET: LEGO1 0x1002c8f0
+	inline virtual MxBool IsA(const char* name) const override // vtable+0x10
+	{
+		return !strcmp(name, Radio::ClassName()) || MxCore::IsA(name);
+	}
 };
 
 #endif // RADIO_H
