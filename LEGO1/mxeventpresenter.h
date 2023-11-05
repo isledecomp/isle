c35b99b2ab0c3b73606b62aa8492a3d3cc3f8772
Joshua Peisach <itzswirlz2020@outlook.com>
2023-11-05 16:57:41 -0500

    MxEventPresenter: AddToManager and small destroy cleanup

--- LEGO1/mxeventpresenter.h
+++ LEGO1/mxeventpresenter.h
@@ -24,6 +24,7 @@ public:
 		return !strcmp(name, MxEventPresenter::ClassName()) || MxMediaPresenter::IsA(name);
 	}
 
+	virtual MxResult AddToManager(); // vtable+0x34
 	virtual void Destroy() override; // vtable+0x38
 
 private:
