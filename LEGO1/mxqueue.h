6643cc0f2819793cae2ceeabd41de22f57693456
disinvite <disinvite@users.noreply.github.com>
2023-11-05 16:09:08 -0500

    Missed clang-format on mxqueue.h

--- LEGO1/mxqueue.h
+++ LEGO1/mxqueue.h
@@ -6,22 +6,21 @@
 template <class T>
 class MxQueue : public MxList<T> {
 public:
-  void Enqueue(T& p_obj)
-  {
-    // TODO
-  }
+	void Enqueue(T& p_obj)
+	{
+		// TODO
+	}
 
-  MxBool Dequeue(T& p_obj)
-  {
-    MxBool has_next = (m_first != NULL);
-    if (m_first) {
-      p_obj = m_first->GetValue();
-      _DeleteEntry(m_first);
-    }
+	MxBool Dequeue(T& p_obj)
+	{
+		MxBool has_next = (m_first != NULL);
+		if (m_first) {
+			p_obj = m_first->GetValue();
+			_DeleteEntry(m_first);
+		}
 
-    return has_next;
-  }
+		return has_next;
+	}
 };
-
 
 #endif // MXQUEUE_H
