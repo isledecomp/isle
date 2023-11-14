6a1e05c2703abab22a9953e3064d74b37c531903
disinvite <disinvite@users.noreply.github.com>
2023-11-14 14:49:27 -0500

    Use isledecomp lib in reccmp

--- tools/isledecomp/isledecomp/dir.py
+++ tools/isledecomp/isledecomp/dir.py
@@ -7,11 +7,15 @@ def file_is_cpp(filename: str) -> bool:
     return ext.lower() in ('.h', '.cpp')
 
 
-def walk_source_dir(source: str) -> Iterator[str]:
+def walk_source_dir(source: str, recursive: bool = True) -> Iterator[str]:
     """Generator to walk the given directory recursively and return
        any C++ files found."""
 
+    source = os.path.abspath(source)
     for subdir, dirs, files in os.walk(source):
         for file in files:
             if file_is_cpp(file):
                 yield os.path.join(subdir, file)
+
+        if not recursive:
+            break
