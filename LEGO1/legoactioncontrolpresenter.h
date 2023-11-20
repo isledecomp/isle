e654b64e5d52ac5e6b6724a31f0273b82429ecd4
MishaProductions <106913236+MishaProductions@users.noreply.github.com>
2023-11-20 15:17:26 -0500

    fix formatting

--- LEGO1/legoactioncontrolpresenter.h
+++ LEGO1/legoactioncontrolpresenter.h
@@ -2,17 +2,14 @@
 #define LEGOACTIONCONTROLPRESENTER_H
 
 #include "decomp.h"
-#include "mxmediapresenter.h"
 #include "legoutil.h"
+#include "mxmediapresenter.h"
 
 // VTABLE 0x100d5118
 // SIZE 0x68
 class LegoActionControlPresenter : public MxMediaPresenter {
 public:
-  inline LegoActionControlPresenter()
-  {
-	m_unk0x50 = ExtraActionType_none;
-  }
+  inline LegoActionControlPresenter() { m_unk0x50 = ExtraActionType_none; }
   virtual ~LegoActionControlPresenter() override; // vtable+0x00
   // OFFSET: LEGO1 0x1000d0e0
   inline virtual const char *ClassName() const override // vtable+0x0c
@@ -29,11 +26,11 @@ public:
   }
   virtual void ReadyTickle() override;     // vtable+0x18
   virtual void RepeatingTickle() override; // vtable+0x24
-  virtual void ParseExtra(); // vtable+0x30
+  virtual void ParseExtra();               // vtable+0x30
   virtual MxResult AddToManager() override;
 
 private:
-  virtual void Destroy(MxBool p_fromDestructor); //vtable+0x5c
+  virtual void Destroy(MxBool p_fromDestructor); // vtable+0x5c
   ExtraActionType m_unk0x50;
   MxString m_unk0x54;
   undefined4 m_unk0x64;
