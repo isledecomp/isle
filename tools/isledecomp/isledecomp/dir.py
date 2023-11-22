15f716708c4130ac3824c1d962417573bf586b18
tntexplosivesltd <tntexplosivesltd@gmail.com>
2023-11-22 21:51:57 +1300

    Add pylint and black

--- tools/isledecomp/isledecomp/dir.py
+++ tools/isledecomp/isledecomp/dir.py
@@ -4,12 +4,12 @@ from typing import Iterator
 
 def is_file_cpp(filename: str) -> bool:
     (_, ext) = os.path.splitext(filename)
-    return ext.lower() in ('.h', '.cpp')
+    return ext.lower() in (".h", ".cpp")
 
 
 def walk_source_dir(source: str, recursive: bool = True) -> Iterator[str]:
     """Generator to walk the given directory recursively and return
-       any C++ files found."""
+    any C++ files found."""
 
     source = os.path.abspath(source)
     for subdir, _, files in os.walk(source):
