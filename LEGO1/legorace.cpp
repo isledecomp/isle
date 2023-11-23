6fd4c3d01a72e87f34b4ec9fcb5902d0cce0f348
Joshua Peisach <itzswirlz2020@outlook.com>
2023-11-23 13:08:17 -0500

    fix function ordering

--- LEGO1/legorace.cpp
+++ LEGO1/legorace.cpp
@@ -45,20 +45,6 @@ LegoRace::LegoRace()
 	NotificationManager()->Register(this);
 }
 
-// OFFSET: LEGO1 0x10015d40 STUB
-LegoRace::~LegoRace()
-{
-	// TODO
-}
-
-// OFFSET: LEGO1 0x10015e00 STUB
-MxLong LegoRace::Notify(MxParam& p)
-{
-	// TODO
-
-	return 0;
-}
-
 // OFFSET: LEGO1 0x10015b70
 undefined4 LegoRace::vtable0x70()
 {
@@ -75,6 +61,20 @@ undefined4 LegoRace::vtable0x74()
 MxBool LegoRace::vtable0x64()
 {
 	return FALSE;
+}
+
+// OFFSET: LEGO1 0x10015d40 STUB
+LegoRace::~LegoRace()
+{
+	// TODO
+}
+
+// OFFSET: LEGO1 0x10015e00 STUB
+MxLong LegoRace::Notify(MxParam& p)
+{
+	// TODO
+
+	return 0;
 }
 
 // OFFSET: LEGO1 0x10015ed0 STUB
