786bd4b6fa305bb2f184b0f7fa15e9487a5bf2bb
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-08 02:34:41 +0100

    py-fixes

--- tools/verexp/verexp.py
+++ tools/verexp/verexp.py
@@ -8,61 +8,71 @@ import sys
 
 from isledecomp.utils import print_diff
 
-parser = argparse.ArgumentParser(
-    allow_abbrev=False, description="Verify Exports: Compare the exports of two DLLs."
-)
-parser.add_argument("original", metavar="original-binary", help="The original binary")
-parser.add_argument(
-    "recompiled", metavar="recompiled-binary", help="The recompiled binary"
-)
-parser.add_argument(
-    "--no-color", "-n", action="store_true", help="Do not color the output"
-)
 
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(
+        allow_abbrev=False,
+        description="Verify Exports: Compare the exports of two DLLs.",
+    )
+    parser.add_argument(
+        "original", metavar="original-binary", help="The original binary"
+    )
+    parser.add_argument(
+        "recompiled", metavar="recompiled-binary", help="The recompiled binary"
+    )
+    parser.add_argument(
+        "--no-color", "-n", action="store_true", help="Do not color the output"
+    )
 
-if not os.path.isfile(args.original):
-    parser.error(f"Original binary file {args.original} does not exist")
+    args = parser.parse_args()
 
-if not os.path.isfile(args.recompiled):
-    parser.error(f"Recompiled binary {args.recompiled} does not exist")
+    if not os.path.isfile(args.original):
+        parser.error(f"Original binary file {args.original} does not exist")
+
+    if not os.path.isfile(args.recompiled):
+        parser.error(f"Recompiled binary {args.recompiled} does not exist")
+
+    def get_file_in_script_dir(fn):
+        return os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), fn)
+
+    def get_exports(file):
+        call = [get_file_in_script_dir("DUMPBIN.EXE"), "/EXPORTS"]
+
+        if os.name != "nt":
+            call.insert(0, "wine")
+            file = (
+                subprocess.check_output(["winepath", "-w", file])
+                .decode("utf-8")
+                .strip()
+            )
+
+        call.append(file)
+
+        raw = subprocess.check_output(call).decode("utf-8").split("\r\n")
+        exports = []
+
+        start = False
+
+        for line in raw:
+            if not start:
+                if line == "            ordinal hint   name":
+                    start = True
+            else:
+                if line:
+                    exports.append(line[27 : line.rindex("  (")])
+                elif exports:
+                    break
+
+        return exports
+
+    og_exp = get_exports(args.original)
+    re_exp = get_exports(args.recompiled)
+
+    udiff = difflib.unified_diff(og_exp, re_exp)
+    has_diff = print_diff(udiff, args.no_color)
+
+    return 1 if has_diff else 0
 
 
-def get_file_in_script_dir(fn):
-    return os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), fn)
-
-
-def get_exports(file):
-    call = [get_file_in_script_dir("DUMPBIN.EXE"), "/EXPORTS"]
-
-    if os.name != "nt":
-        call.insert(0, "wine")
-        file = subprocess.check_output(["winepath", "-w", file]).decode("utf-8").strip()
-
-    call.append(file)
-
-    raw = subprocess.check_output(call).decode("utf-8").split("\r\n")
-    exports = []
-
-    start = False
-
-    for line in raw:
-        if not start:
-            if line == "            ordinal hint   name":
-                start = True
-        else:
-            if line:
-                exports.append(line[27 : line.rindex("  (")])
-            elif exports:
-                break
-
-    return exports
-
-
-og_exp = get_exports(args.original)
-re_exp = get_exports(args.recompiled)
-
-udiff = difflib.unified_diff(og_exp, re_exp)
-has_diff = print_diff(udiff, args.no_color)
-
-sys.exit(1 if has_diff else 0)
+if __name__ == "__main__":
+    raise SystemExit(main())
