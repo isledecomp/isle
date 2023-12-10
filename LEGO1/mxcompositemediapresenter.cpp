b18e854cbcb7cfd42279060ff7bd3dcd4f7acf50
Christian Semmler <mail@csemmler.com>
2023-12-10 10:01:42 -0500

    Bootstrap MxCompositeMediaPresenter

--- LEGO1/mxcompositemediapresenter.cpp
+++ LEGO1/mxcompositemediapresenter.cpp
@@ -1,7 +1,47 @@
 #include "mxcompositemediapresenter.h"
 
-// STUB: LEGO1 0x10073ea0
+#include "legoomni.h"
+#include "legovideomanager.h"
+
+DECOMP_SIZE_ASSERT(MxCompositeMediaPresenter, 0x50)
+
+// FUNCTION: LEGO1 0x10073ea0
 MxCompositeMediaPresenter::MxCompositeMediaPresenter()
 {
+	m_unk4c = 0;
+	m_unk4e = 0;
+	VideoManager()->AddPresenter(*this);
+}
+
+// FUNCTION: LEGO1 0x10074020
+MxCompositeMediaPresenter::~MxCompositeMediaPresenter()
+{
+	VideoManager()->RemovePresenter(*this);
+}
+
+// STUB: LEGO1 0x10074090
+MxResult MxCompositeMediaPresenter::StartAction(MxStreamController* p_controller, MxDSAction* p_action)
+{
 	// TODO
+	return SUCCESS;
+}
+
+// STUB: LEGO1 0x100742e0
+void MxCompositeMediaPresenter::StartingTickle()
+{
+	// TODO
+}
+
+// STUB: LEGO1 0x10074470
+MxResult MxCompositeMediaPresenter::Tickle()
+{
+	// TODO
+	return SUCCESS;
+}
+
+// STUB: LEGO1 0x10074540
+MxResult MxCompositeMediaPresenter::PutData()
+{
+	// TODO
+	return SUCCESS;
 }
