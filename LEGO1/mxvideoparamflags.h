92468b92af3e0f5a30a0422fcb41ecd82f75f9fc
Christian Semmler <mail@csemmler.com>
2023-12-12 11:46:43 -0500

    Fix last issue

--- LEGO1/mxvideoparamflags.h
+++ LEGO1/mxvideoparamflags.h
@@ -29,19 +29,19 @@ public:
 	inline BYTE GetFullScreen() { return m_flags1.m_bit0; }
 	inline BYTE GetFlipSurfaces() { return m_flags1.m_bit1; }
 	inline BYTE GetBackBuffers() { return m_flags1.m_bit2; }
-	inline BYTE Get_f1bit3() { return m_flags1.m_bit3; }
-	inline BYTE Get_f1bit4() { return m_flags1.m_bit4; }
+	inline BYTE GetF1bit3() { return m_flags1.m_bit3; }
+	inline BYTE GetF1bit4() { return m_flags1.m_bit4; }
 	inline BYTE Get16Bit() { return m_flags1.m_bit5; }
 	inline BYTE GetWideViewAngle() { return m_flags1.m_bit6; }
-	inline BYTE Get_f1bit7() { return m_flags1.m_bit7; }
-	inline BYTE Get_f2bit0() { return m_flags2.m_bit0; }
-	inline BYTE Get_f2bit1() { return m_flags2.m_bit1; }
-	inline BYTE Get_f2bit2() { return m_flags2.m_bit2; }
-	inline BYTE Get_f2bit3() { return m_flags2.m_bit3; }
-	inline BYTE Get_f2bit4() { return m_flags2.m_bit4; }
-	inline BYTE Get_f2bit5() { return m_flags2.m_bit5; }
-	inline BYTE Get_f2bit6() { return m_flags2.m_bit6; }
-	inline BYTE Get_f2bit7() { return m_flags2.m_bit7; }
+	inline BYTE GetF1bit7() { return m_flags1.m_bit7; }
+	inline BYTE GetF2bit0() { return m_flags2.m_bit0; }
+	inline BYTE GetF2bit1() { return m_flags2.m_bit1; }
+	inline BYTE GetF2bit2() { return m_flags2.m_bit2; }
+	inline BYTE GetF2bit3() { return m_flags2.m_bit3; }
+	inline BYTE GetF2bit4() { return m_flags2.m_bit4; }
+	inline BYTE GetF2bit5() { return m_flags2.m_bit5; }
+	inline BYTE GetF2bit6() { return m_flags2.m_bit6; }
+	inline BYTE GetF2bit7() { return m_flags2.m_bit7; }
 
 private:
 	FlagBitfield m_flags1;
