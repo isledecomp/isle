0278d032b912d65d1c3aec388cfe57c3ec44a998
Christian Semmler <mail@csemmler.com>
2023-12-13 07:53:17 -0500

    Bootstrap MxCompositeMediaPresenter (#321)
    * Bootstrap MxCompositeMediaPresenter
    
    * Fixes

--- LEGO1/mxcompositemediapresenter.h
+++ LEGO1/mxcompositemediapresenter.h
@@ -3,11 +3,14 @@
 
 #include "mxcompositepresenter.h"
 
-// VTABLE: LEGO1 0x100dc618
+// VTABLE: LEGO1 0x100d96b0
 // SIZE 0x50
 class MxCompositeMediaPresenter : public MxCompositePresenter {
 public:
 	MxCompositeMediaPresenter();
+	virtual ~MxCompositeMediaPresenter() override;
+
+	virtual MxResult Tickle() override; // vtable+0x08
 
 	// FUNCTION: LEGO1 0x10073f10
 	inline virtual const char* ClassName() const override // vtable+0x0c
@@ -21,6 +24,17 @@ public:
 	{
 		return !strcmp(p_name, MxCompositeMediaPresenter::ClassName()) || MxCompositePresenter::IsA(p_name);
 	}
+
+	virtual void StartingTickle() override;                                           // vtable+0x1c
+	virtual MxResult StartAction(MxStreamController*, MxDSAction* p_action) override; // vtable+0x3c
+	virtual MxResult PutData();                                                       // vtable+0x4c
+
+private:
+	undefined2 m_unk0x4c; // 0x4c
+	undefined m_unk0x4e;  // 0x4e
 };
+
+// SYNTHETIC: LEGO1 0x10074000
+// MxCompositeMediaPresenter::`scalar deleting destructor'
 
 #endif // MXCOMPOSITEMEDIAPRESENTER_H
