0eada87b93d2d5dedc9326825c6d880058a65e13
Joshua Peisach <itzswirlz2020@outlook.com>
2023-12-15 11:41:07 -0500

    Bootstrap MxControlPresenter  (#332)
    * Start MxControlPresenter vtable functions
    
    * Add changes
    
    ---------
    
    Co-authored-by: Christian Semmler <mail@csemmler.com>

--- LEGO1/mxcontrolpresenter.h
+++ LEGO1/mxcontrolpresenter.h
@@ -24,15 +24,31 @@ public:
 		return !strcmp(p_name, MxControlPresenter::ClassName()) || MxCompositePresenter::IsA(p_name);
 	}
 
-	virtual void ReadyTickle() override; // vtable+0x18
+	virtual void ReadyTickle() override;                                     // vtable+0x18
+	virtual void RepeatingTickle() override;                                 // vtable+0x24
+	virtual void ParseExtra() override;                                      // vtable+0x30
+	virtual MxResult AddToManager() override;                                // vtable+0x34
+	virtual MxResult StartAction(MxStreamController*, MxDSAction*) override; // vtable+0x3c
+	virtual void EndAction() override;                                       // vtable+0x40
+	virtual MxBool HasTickleStatePassed(TickleState p_tickleState) override; // vtable+0x48
+	virtual void Enable(MxBool p_enable) override;                           // vtable+0x54
+	virtual MxBool VTable0x64(undefined4 p_undefined) override;              // vtable+0x64
+	virtual void VTable0x68(MxBool p_undefined);                             // vtable+0x64
 
 private:
-	undefined2 m_unk0x4c;
-	MxS16 m_unk0x4e;
-	undefined m_unk0x50;
-	undefined2 m_unk0x52;
-	undefined2 m_unk0x54;
-	undefined4* m_unk0x58;
+	MxBool FUN_10044270(undefined4, undefined4, undefined4*);
+	MxBool FUN_10044480(undefined4, undefined4*);
+	void FUN_10044540(undefined2);
+
+	undefined2 m_unk0x4c;  // 0x4c
+	MxS16 m_unk0x4e;       // 0x4e
+	MxBool m_unk0x50;      // 0x50
+	undefined2 m_unk0x52;  // 0x52
+	undefined2 m_unk0x54;  // 0x54
+	undefined4* m_unk0x58; // 0x58
 };
+
+// SYNTHETIC: LEGO1 0x100440f0
+// MxControlPresenter::`scalar deleting destructor'
 
 #endif // MXCONTROLPRESENTER_H
