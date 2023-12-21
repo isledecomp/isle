44c08acc52b6e3ad77ad3b3f99c858b9afe6f6c8
Misha <106913236+MishaProductions@users.noreply.github.com>
2023-12-21 11:38:04 -0500

    fix 2

---
+++ LEGO1/mxunknown100dbdbc.cpp
@@ -0,0 +1,11 @@
+#include "mxunknown100dbdbc.h"
+
+#include "decomp.h"
+
+DECOMP_SIZE_ASSERT(MxUnknown100dbdbc, 0x14)
+
+// STUB: LEGO1 0x100a6fd0
+MxUnknown100dbdbc::MxUnknown100dbdbc()
+{
+	// TODO
+}
