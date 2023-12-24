43efcc5253d9f4ae1e160053fb1e9992a03f0ece
Joshua Peisach <itzswirlz2020@outlook.com>
2023-12-24 09:16:07 -0500

    jukebox ctor (#356)
    * jukebox ctor
    
    * naming conventions
    
    * Fixes
    
    ---------
    
    Co-authored-by: Christian Semmler <mail@csemmler.com>

--- LEGO1/jukebox.cpp
+++ LEGO1/jukebox.cpp
@@ -1,7 +1,14 @@
 #include "jukebox.h"
 
-// STUB: LEGO1 0x1005d660
+#include "mxnotificationmanager.h"
+#include "mxomni.h"
+
+DECOMP_SIZE_ASSERT(JukeBox, 0x104)
+
+// FUNCTION: LEGO1 0x1005d660
 JukeBox::JukeBox()
 {
-	// TODO
+	m_unk0x100 = 0;
+	m_unk0xfc = 0;
+	NotificationManager()->Register(this);
 }
--- LEGO1/jukebox.h
+++ LEGO1/jukebox.h
@@ -1,6 +1,7 @@
 #ifndef JUKEBOX_H
 #define JUKEBOX_H
 
+#include "decomp.h"
 #include "legoworld.h"
 
 // VTABLE: LEGO1 0x100d8958
@@ -21,6 +22,11 @@ public:
 	{
 		return !strcmp(p_name, JukeBox::ClassName()) || LegoWorld::IsA(p_name);
 	}
+
+private:
+	undefined m_unk0xf8[4]; // 0xf8
+	undefined4 m_unk0xfc;   // 0xfc
+	undefined2 m_unk0x100;  // 0x100
 };
 
 #endif // JUKEBOX_H
