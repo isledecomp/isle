f260838b7f27f329ec9a26632e2d23e40ffd6bc4
Joshua Peisach <itzswirlz2020@outlook.com>
2023-12-25 14:35:32 -0500

    LegoLoadCacheSoundPresenter ctor, dtor, init, destroy

--- LEGO1/legoloadcachesoundpresenter.h
+++ LEGO1/legoloadcachesoundpresenter.h
@@ -1,6 +1,7 @@
 #ifndef LEGOLOADCACHESOUNDPRESENTER_H
 #define LEGOLOADCACHESOUNDPRESENTER_H
 
+#include "decomp.h"
 #include "mxwavepresenter.h"
 
 // VTABLE: LEGO1 0x100d5fa8
@@ -19,6 +20,15 @@ public:
 
 private:
 	void Init();
+	void Destroy(MxBool p_fromDestructor);
+
+	// TODO: Types
+	undefined4* m_unk0x6c;
+	undefined4* m_unk0x70; // might be a void* as per Destroy function
+	undefined4 m_unk0x74;
+	undefined4 m_unk0x78;
+	undefined m_unk0x7c;
+	undefined4 m_unk0x7d[4];
 };
 
 #endif // LEGOLOADCACHESOUNDPRESENTER_H
