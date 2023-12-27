d5854a46aed638b5d646022976327c8d480044c7
Joshua Peisach <itzswirlz2020@outlook.com>
2023-12-27 13:36:32 -0500

    HospitalState ctor (#375)
    * HospitalState ctor
    
    * Add member offsets in HospitalState header
    
    * Remove unhelpful comment
    
    ---------
    
    Co-authored-by: Christian Semmler <mail@csemmler.com>

--- LEGO1/hospitalstate.h
+++ LEGO1/hospitalstate.h
@@ -1,6 +1,7 @@
 #ifndef HOSPITALSTATE_H
 #define HOSPITALSTATE_H
 
+#include "decomp.h"
 #include "legostate.h"
 
 // VTABLE: LEGO1 0x100d97a0
@@ -21,6 +22,15 @@ public:
 	{
 		return !strcmp(p_name, HospitalState::ClassName()) || LegoState::IsA(p_name);
 	}
+
+private:
+	undefined m_unk0x8[4]; // 0x8
+	undefined2 m_unk0xc;   // 0xc
+	undefined2 m_unk0xe;   // 0xe
+	undefined2 m_unk0x10;  // 0x10
+	undefined2 m_unk0x12;  // 0x12
+	undefined2 m_unk0x14;  // 0x14
+	undefined2 m_unk0x16;  // 0x16
 };
 
 #endif // HOSPITALSTATE_H
