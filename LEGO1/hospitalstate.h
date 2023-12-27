d1fb7e1ba957c83319931ccd1c0449ca9cd24623
Joshua Peisach <itzswirlz2020@outlook.com>
2023-12-27 13:28:40 -0500

    Add member offsets in HospitalState header

--- LEGO1/hospitalstate.h
+++ LEGO1/hospitalstate.h
@@ -24,14 +24,14 @@ public:
 	}
 
 private:
-	// TODO: Figure out unused type
-	undefined m_unk0x8[4];
-	undefined2 m_unk0xc;
-	undefined2 m_unk0xe;
-	undefined2 m_unk0x10;
-	undefined2 m_unk0x12;
-	undefined2 m_unk0x14;
-	undefined2 m_unk0x16;
+	// TODO: Figure out unused types
+	undefined m_unk0x8[4]; // 0x8
+	undefined2 m_unk0xc;   // 0xc
+	undefined2 m_unk0xe;   // 0xe
+	undefined2 m_unk0x10;  // 0x10
+	undefined2 m_unk0x12;  // 0x12
+	undefined2 m_unk0x14;  // 0x14
+	undefined2 m_unk0x16;  // 0x16
 };
 
 #endif // HOSPITALSTATE_H
