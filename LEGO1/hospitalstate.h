6fb7eec39c057706a563a68a73d5a55268d858c7
Joshua Peisach <itzswirlz2020@outlook.com>
2023-12-27 13:26:57 -0500

    HospitalState ctor

--- LEGO1/hospitalstate.h
+++ LEGO1/hospitalstate.h
@@ -1,6 +1,7 @@
 #ifndef HOSPITALSTATE_H
 #define HOSPITALSTATE_H
 
+#include "decomp.h"
 #include "legostate.h"
 
 // VTABLE: LEGO1 0x100d97a0
@@ -21,6 +22,16 @@ public:
 	{
 		return !strcmp(p_name, HospitalState::ClassName()) || LegoState::IsA(p_name);
 	}
+
+private:
+	// TODO: Figure out unused type
+	undefined m_unk0x8[4];
+	undefined2 m_unk0xc;
+	undefined2 m_unk0xe;
+	undefined2 m_unk0x10;
+	undefined2 m_unk0x12;
+	undefined2 m_unk0x14;
+	undefined2 m_unk0x16;
 };
 
 #endif // HOSPITALSTATE_H
