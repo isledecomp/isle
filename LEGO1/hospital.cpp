4f5bf7f2f2d151fc2056a9be46294dd6db147614
Joshua Peisach <itzswirlz2020@outlook.com>
2024-01-01 14:09:58 -0500

    Hospital ctor

--- LEGO1/hospital.cpp
+++ LEGO1/hospital.cpp
@@ -1,9 +1,26 @@
 #include "hospital.h"
 
-// STUB: LEGO1 0x100745e0
+#include "mxnotificationmanager.h"
+#include "mxomni.h"
+
+DECOMP_SIZE_ASSERT(Hospital, 0x12c)
+
+// FUNCTION: LEGO1 0x100745e0
 Hospital::Hospital()
 {
-	// TODO
+	this->m_unk0xf8 = 0;
+	this->m_unk0x100 = 0;
+	this->m_unk0x104 = 0;
+	this->m_unk0x108 = 0;
+	this->m_unk0xfc = 0;
+	this->m_unk0x10c = 0;
+	this->m_unk0x110 = 0;
+	this->m_unk0x114 = 0;
+	this->m_unk0x118 = 0;
+	this->m_unk0x11c = 0;
+	this->m_unk0x120 = 0;
+	this->m_unk0x128 = 0;
+	NotificationManager()->Register(this);
 }
 
 // STUB: LEGO1 0x100747f0
