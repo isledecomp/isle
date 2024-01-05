4895cc165c2255290f1b21ab921c56dbb3152790
Misha <106913236+MishaProductions@users.noreply.github.com>
2024-01-04 20:56:11 -0400

    Update legomodelpresenter.cpp

--- LEGO1/legomodelpresenter.cpp
+++ LEGO1/legomodelpresenter.cpp
@@ -3,16 +3,16 @@
 // GLOBAL: LEGO1 0x100f7ae0
 int g_modelPresenterConfig = 1;
 
-// FUNCTION: LEGO1 0x1007f660
-void LegoModelPresenter::configureLegoModelPresenter(MxS32 p_modelPresenterConfig)
-{
-	g_modelPresenterConfig = p_modelPresenterConfig;
-}
-
 // FUNCTION: LEGO1 0x1000cca0
 void LegoModelPresenter::Destroy()
 {
 	Destroy(FALSE);
+}
+
+// FUNCTION: LEGO1 0x1007f660
+void LegoModelPresenter::configureLegoModelPresenter(MxS32 p_modelPresenterConfig)
+{
+	g_modelPresenterConfig = p_modelPresenterConfig;
 }
 
 // STUB: LEGO1 0x1007f670
