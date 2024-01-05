c4502eb6fd4c082438daa9641b988485c8fdaa62
Misha <106913236+MishaProductions@users.noreply.github.com>
2024-01-05 12:28:53 -0400

    Update mxactionnotificationparam.cpp

--- LEGO1/mxactionnotificationparam.cpp
+++ LEGO1/mxactionnotificationparam.cpp
@@ -18,7 +18,12 @@ MxNotificationParam* MxEndActionNotificationParam::Clone()
 // FUNCTION: LEGO1 0x100b0300
 MxNotificationParam* MxStartActionNotificationParam::Clone()
 {
-	return new MxStartActionNotificationParam(c_notificationStartAction, this->m_sender, this->m_action, this->m_realloc);
+	return new MxStartActionNotificationParam(
+		c_notificationStartAction,
+		this->m_sender,
+		this->m_action,
+		this->m_realloc
+	);
 }
 
 // FUNCTION: LEGO1 0x100b04f0
