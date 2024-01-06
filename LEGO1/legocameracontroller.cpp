cdb69676f4f60bdc8cd0a9992f7cda8f66297b5f
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-01-06 12:49:46 +0100

    LegoCameraController: return references to static data in stub functions

--- LEGO1/legocameracontroller.cpp
+++ LEGO1/legocameracontroller.cpp
@@ -25,17 +25,20 @@ void LegoCameraController::FUN_100123e0(Matrix4Data& p_transform, MxU32)
 // STUB: LEGO1 0x10012740
 Vector3Data& LegoCameraController::FUN_10012740()
 {
-	return Vector3Data();
+	static Vector3Data v;
+	return v;
 }
 
 // STUB: LEGO1 0x100127f0
 Vector3Data& LegoCameraController::FUN_100127f0()
 {
-	return Vector3Data();
+	static Vector3Data v;
+	return v;
 }
 
 // STUB: LEGO1 0x100128a0
 Vector3Data& LegoCameraController::FUN_100128a0()
 {
-	return Vector3Data();
+	static Vector3Data v;
+	return v;
 }
