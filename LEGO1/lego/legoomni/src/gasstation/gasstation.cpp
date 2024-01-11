102734c970e455fbe4f029060f9816433994fbef
Daniel Hejduk <danielhejduk@disroot.org>
2024-01-11 18:24:38 +0100

    Implementing GasStation:GasStation() function

--- LEGO1/lego/legoomni/src/gasstation/gasstation.cpp
+++ LEGO1/lego/legoomni/src/gasstation/gasstation.cpp
@@ -3,7 +3,16 @@
 // STUB: LEGO1 0x100046a0
 GasStation::GasStation()
 {
-	// TODO
+	field1_0xf8 = 0;
+	field6_0x104 = 0;
+	field5_0x100 = 0;
+	field7_0x106 = 0;
+	field4_0xfc = 0;
+	field8_0x108 = 0;
+	field_0x114 = 0;
+	field9_0x10c = 0;
+	field_0x115 = 0;
+	field10_0x110 = 0;
 }
 
 // STUB: LEGO1 0x100048c0
