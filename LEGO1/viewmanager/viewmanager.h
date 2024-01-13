f55a2e36869581072de7be89c64af8372872d53e
Christian Semmler <mail@csemmler.com>
2024-01-13 14:40:55 -0500

    Match Lego3DView::Create

--- LEGO1/viewmanager/viewmanager.h
+++ LEGO1/viewmanager/viewmanager.h
@@ -7,12 +7,17 @@
 // SIZE 0x1bc
 class ViewManager {
 public:
-	ViewManager(Tgl::Group* scene, const OrientableROI* point_of_view);
+	ViewManager(Tgl::Renderer* pRenderer, Tgl::Group* scene, const OrientableROI* point_of_view);
 	virtual ~ViewManager();
 
 	__declspec(dllexport) void RemoveAll(ViewROI*);
 
 	void SetPOVSource(const OrientableROI* point_of_view);
+	void SetResolution(int width, int height);
+	void SetFrustrum(float fov, float front, float back);
+
+private:
+	undefined m_pad[0x1b8];
 };
 
 #endif // VIEWMANAGER_H
