f6248d12d927b474ab24ef3d0e46e02140186924
Christian Semmler <mail@csemmler.com>
2024-01-14 13:05:30 -0500

    Rename annotation

--- LEGO1/omni/include/mxomnicreateparam.h
+++ LEGO1/omni/include/mxomnicreateparam.h
@@ -31,7 +31,7 @@ private:
 	MxOmniCreateFlags m_createFlags;
 };
 
-// FUNCTION: ISLE 0x4014b0
+// SYNTHETIC: ISLE 0x4014b0
 // MxOmniCreateParam::~MxOmniCreateParam
 
 #endif // MXOMNICREATEPARAM_H
