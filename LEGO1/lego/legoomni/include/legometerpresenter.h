7f72a36462643ce890ba05ad017d03d1e8297c9c
Misha <106913236+MishaProductions@users.noreply.github.com>
2024-01-15 10:26:52 -0500

    Implement LegoMeterPresenter (#439)
    * push changes
    
    * Update legometerpresenter.h
    
    * fix wrong offset
    
    * fix deconstructor
    
    * fix + improve accuracy
    
    * Update legometerpresenter.cpp
    
    * Match functions
    
    ---------
    
    Co-authored-by: Christian Semmler <mail@csemmler.com>

--- LEGO1/lego/legoomni/include/legometerpresenter.h
+++ LEGO1/lego/legoomni/include/legometerpresenter.h
@@ -7,7 +7,30 @@
 // SIZE 0x94 (from 0x1000a163)
 class LegoMeterPresenter : public MxStillPresenter {
 public:
+	LegoMeterPresenter();
+	virtual ~LegoMeterPresenter() override;
+
 	// MxStillPresenter's `::ClassName` and `::IsA` are used.
+
+	virtual void StreamingTickle() override; // vtable+0x20
+	virtual void RepeatingTickle() override; // vtable+0x24
+	virtual void ParseExtra() override;      // vtable+0x30
+
+private:
+	void FUN_10043a50();
+
+	MxU8* m_unk0x6c;     // 0x6c
+	MxU16 m_type;        // 0x70
+	MxString m_variable; // 0x74
+	MxFloat m_unk0x84;   // 0x84
+	MxU16 m_unk0x88;     // 0x88
+	MxU16 m_unk0x8a;     // 0x8a
+	MxU16 m_unk0x8c;     // 0x8c
+	MxU16 m_unk0x8e;     // 0x8e
+	MxU16 m_layout;      // 0x90
 };
+
+// SYNTHETIC: LEGO1 0x10043760
+// LegoMeterPresenter::`scalar deleting destructor'
 
 #endif // LEGOMETERPRESENTER_H
