63e85438dd42ad0e113f49358f9bf041a69897fc
Misha <106913236+MishaProductions@users.noreply.github.com>
2024-01-18 20:11:39 -0500

    Update infocenterdoor.cpp

--- LEGO1/lego/legoomni/src/infocenter/infocenterdoor.cpp
+++ LEGO1/lego/legoomni/src/infocenter/infocenterdoor.cpp
@@ -28,20 +28,6 @@ MxLong InfocenterDoor::Notify(MxParam& p_param)
 	return LegoWorld::Notify(p_param);
 }
 
-// FUNCTION: LEGO1 0x10037a70
-void InfocenterDoor::VTable0x50()
-{
-	LegoWorld::VTable0x50();
-	PlayMusic(11);
-	FUN_10015820(FALSE, LegoOmni::c_disableInput | LegoOmni::c_disable3d | LegoOmni::c_clearScreen);
-}
-
-// STUB: LEGO1 0x10037cd0
-MxBool InfocenterDoor::VTable0x64()
-{
-	return TRUE;
-}
-
 // FUNCTION: LEGO1 0x10037c80
 void InfocenterDoor::VTable0x68(MxBool p_add)
 {
@@ -56,4 +42,18 @@ void InfocenterDoor::VTable0x68(MxBool p_add)
 			InputManager()->ClearWorld();
 		}
 	}
+}
+
+// FUNCTION: LEGO1 0x10037a70
+void InfocenterDoor::VTable0x50()
+{
+	LegoWorld::VTable0x50();
+	PlayMusic(11);
+	FUN_10015820(FALSE, LegoOmni::c_disableInput | LegoOmni::c_disable3d | LegoOmni::c_clearScreen);
+}
+
+// STUB: LEGO1 0x10037cd0
+MxBool InfocenterDoor::VTable0x64()
+{
+	return TRUE;
 }
