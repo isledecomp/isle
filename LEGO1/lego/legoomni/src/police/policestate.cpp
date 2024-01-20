37b068e2df223fdb8d1bc58c4c29a40532508023
Joshua Peisach <itzswirlz2020@outlook.com>
2024-01-20 13:34:29 -0500

    Implmement PoliceState::VTable0x1c

--- LEGO1/lego/legoomni/src/police/policestate.cpp
+++ LEGO1/lego/legoomni/src/police/policestate.cpp
@@ -10,3 +10,18 @@ PoliceState::PoliceState()
 	m_unk0xc = 0;
 	m_unk0x8 = (rand() % 2 == 0) ? 501 : 500;
 }
+
+// FUNCTION: LEGO1 0x1005e990
+MxResult PoliceState::VTable0x1c(LegoFileStream* p_legoFileStream)
+{
+	if (p_legoFileStream->IsWriteMode()) {
+		p_legoFileStream->FUN_10006030(this->ClassName());
+	}
+	if (p_legoFileStream->IsReadMode()) {
+		p_legoFileStream->Write(&m_unk0x8, sizeof(MxU32));
+	}
+	else if (p_legoFileStream->IsWriteMode()) {
+		p_legoFileStream->Read(&m_unk0x8, sizeof(MxU32));
+	}
+	return SUCCESS;
+}
