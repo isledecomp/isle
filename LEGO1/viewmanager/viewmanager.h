9a77a2a10b78e1c26da4eb3016514db94a5ae207
Misha <106913236+MishaProductions@users.noreply.github.com>
2024-01-20 14:57:13 -0500

    Implement Lego3DView::Render

--- LEGO1/viewmanager/viewmanager.h
+++ LEGO1/viewmanager/viewmanager.h
@@ -15,6 +15,7 @@ public:
 	void SetPOVSource(const OrientableROI* point_of_view);
 	void SetResolution(int width, int height);
 	void SetFrustrum(float fov, float front, float back);
+	void Update(float p_previousRenderTime, float p_und2);
 
 	// SYNTHETIC: LEGO1 0x100a6000
 	// ViewManager::`scalar deleting destructor'
