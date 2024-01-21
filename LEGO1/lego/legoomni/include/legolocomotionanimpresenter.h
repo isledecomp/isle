3f35302e7654d3d0b0e17c8c61b6e028f73373e8
Misha <106913236+MishaProductions@users.noreply.github.com>
2024-01-21 09:58:15 -0500

    begin LegoLocomotionAnimPresenter (#474)
    * LegoLocomotionAnimPresenter
    
    * Fix game startup
    
    * Fixes
    
    * Add size assert
    
    ---------
    
    Co-authored-by: Christian Semmler <mail@csemmler.com>

--- LEGO1/lego/legoomni/include/legolocomotionanimpresenter.h
+++ LEGO1/lego/legoomni/include/legolocomotionanimpresenter.h
@@ -4,9 +4,11 @@
 #include "legoloopinganimpresenter.h"
 
 // VTABLE: LEGO1 0x100d9170
+// SIZE 0xd8
 class LegoLocomotionAnimPresenter : public LegoLoopingAnimPresenter {
 public:
 	LegoLocomotionAnimPresenter();
+	virtual ~LegoLocomotionAnimPresenter() override;
 
 	// FUNCTION: LEGO1 0x1006ce50
 	inline const char* ClassName() const override // vtable+0xc
@@ -35,6 +37,14 @@ public:
 
 private:
 	void Init();
+	void Destroy(MxBool p_fromDestructor);
+
+	undefined4 m_unk0xc0;  // 0xc0
+	undefined4* m_unk0xc4; // 0xc4
+	MxCore* m_unk0xc8;     // 0xc8
+	MxS32 m_unk0xcc;       // 0xcc
+	MxS32 m_unk0xd0;       // 0xd0
+	undefined2 m_unk0xd4;  // 0xd4
 };
 
 #endif // LEGOLOCOMOTIONANIMPRESENTER_H
