d1b5bbb6a4d543a2e70685607e27e0047c9bffa7
Misha <106913236+MishaProductions@users.noreply.github.com>
2024-01-21 07:47:10 -0500

    LegoLocomotionAnimPresenter

--- LEGO1/lego/legoomni/include/legolocomotionanimpresenter.h
+++ LEGO1/lego/legoomni/include/legolocomotionanimpresenter.h
@@ -4,6 +4,7 @@
 #include "legoloopinganimpresenter.h"
 
 // VTABLE: LEGO1 0x100d9170
+// SIZE 0xD8
 class LegoLocomotionAnimPresenter : public LegoLoopingAnimPresenter {
 public:
 	LegoLocomotionAnimPresenter();
@@ -35,6 +36,14 @@ public:
 
 private:
 	void Init();
+	void Destroy(MxBool p_fromDestructor);
+
+	undefined4 m_unk0xc0;  // 0xc0
+	undefined4* m_unk0xc4; // 0xc4
+	MxCore* m_unk0xc8;     // 0xc8
+	MxS32 m_unk0xcc;       // 0xcc
+	MxS32 m_unk0xd0;       // 0xd0
+	undefined2 m_unk0xd4;  // 0xd4
 };
 
 #endif // LEGOLOCOMOTIONANIMPRESENTER_H
