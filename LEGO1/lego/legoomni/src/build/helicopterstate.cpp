fd28604f3c67464176278e6d209b86720ad0a8a7
Christian Semmler <mail@csemmler.com>
2024-01-26 12:05:53 -0500

    Move functions

--- LEGO1/lego/legoomni/src/build/helicopterstate.cpp
+++ LEGO1/lego/legoomni/src/build/helicopterstate.cpp
@@ -1,14 +1,1 @@
 #include "helicopterstate.h"
-
-// FUNCTION: LEGO1 0x1000e0b0
-MxBool HelicopterState::VTable0x14()
-{
-	return FALSE;
-}
-
-// FUNCTION: LEGO1 0x1000e0c0
-MxBool HelicopterState::SetFlag()
-{
-	this->m_unk0x8 = 0;
-	return TRUE;
-}
