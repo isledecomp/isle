88fd1e658d3d680e57468e482d66bf8c221cbfad
Joshua Peisach <itzswirlz2020@outlook.com>
2024-01-26 12:09:59 -0500

    HelicopterState vtable (#487)
    * HelicopterState vtable
    
    * Move functions
    
    ---------
    
    Co-authored-by: Christian Semmler <mail@csemmler.com>

--- LEGO1/lego/legoomni/include/helicopterstate.h
+++ LEGO1/lego/legoomni/include/helicopterstate.h
@@ -21,8 +21,15 @@ public:
 		return !strcmp(p_name, HelicopterState::ClassName()) || LegoState::IsA(p_name);
 	}
 
-	virtual MxBool VTable0x14() override; // vtable+0x14
-	virtual MxBool SetFlag() override;    // vtable+0x18
+	// FUNCTION: LEGO1 0x1000e0b0
+	virtual MxBool VTable0x14() override { return FALSE; }; // vtable+0x14
+
+	// FUNCTION: LEGO1 0x1000e0c0
+	virtual MxBool SetFlag() override
+	{
+		m_unk0x8 = 0;
+		return TRUE;
+	}; // vtable+0x18
 
 	inline void SetUnknown8(MxU32 p_unk0x8) { m_unk0x8 = p_unk0x8; }
 	inline MxU32 GetUnkown8() { return m_unk0x8; }
--- LEGO1/lego/legoomni/src/build/helicopterstate.cpp
+++ LEGO1/lego/legoomni/src/build/helicopterstate.cpp
@@ -1,15 +1,1 @@
 #include "helicopterstate.h"
-
-// STUB: LEGO1 0x1000e0b0
-MxBool HelicopterState::VTable0x14()
-{
-	// TODO
-	return FALSE;
-}
-
-// STUB: LEGO1 0x1000e0c0
-MxBool HelicopterState::SetFlag()
-{
-	// TODO
-	return FALSE;
-}
