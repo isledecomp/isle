a9f55ff930944a6089f7d7241def6cc20de16895
Christian Semmler <mail@csemmler.com>
2024-01-31 09:25:00 -0500

    WIP Match

--- LEGO1/lego/legoomni/src/infocenter/infocenterdoor.cpp
+++ LEGO1/lego/legoomni/src/infocenter/infocenterdoor.cpp
@@ -87,18 +87,23 @@ void InfocenterDoor::ReadyWorld()
 // FUNCTION: LEGO1 0x10037a90
 MxLong InfocenterDoor::HandleClick(LegoControlManagerEvent& p_param)
 {
+	MxLong result = 0;
+
 	if (p_param.GetUnknown0x28() == 1) {
 		DeleteObjects(&m_atom, 500, 510);
 
 		switch (p_param.GetClickedObjectId()) {
 		case 1:
 			m_unk0xf8 = 13;
+			result = 1;
 			break;
 		case 2:
 			m_unk0xf8 = 5;
+			result = 1;
 			break;
 		case 3:
 			m_unk0xf8 = 2;
+			result = 1;
 			break;
 		case 4:
 			if (GameState()->GetUnknownC()) {
@@ -112,7 +117,6 @@ MxLong InfocenterDoor::HandleClick(LegoControlManagerEvent& p_param)
 					action.SetAtomId(*g_infodoorScript);
 					BackgroundAudioManager()->LowerVolume();
 					Start(&action);
-					return 1;
 				}
 			}
 			else {
@@ -121,15 +125,15 @@ MxLong InfocenterDoor::HandleClick(LegoControlManagerEvent& p_param)
 				action.SetAtomId(*g_infodoorScript);
 				BackgroundAudioManager()->LowerVolume();
 				Start(&action);
-				return 1;
 			}
-		default:
-			return 0;
+			result = 1;
+			break;
 		}
+
 		TransitionManager()->StartTransition(MxTransitionManager::e_pixelation, 50, FALSE, FALSE);
 	}
 
-	return 1;
+	return result;
 }
 
 // FUNCTION: LEGO1 0x10037c80
