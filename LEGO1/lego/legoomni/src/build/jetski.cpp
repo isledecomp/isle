d5383877467f72b630e551c354c1b336775e7542
Daniel Hejduk <156322098+danielhejduk@users.noreply.github.com>
2024-02-04 19:37:05 +0100

    Removing this-> from jetski.cpp

--- LEGO1/lego/legoomni/src/build/jetski.cpp
+++ LEGO1/lego/legoomni/src/build/jetski.cpp
@@ -5,9 +5,9 @@ DECOMP_SIZE_ASSERT(Jetski, 0x164);
 // FUNCTION: LEGO1 0x1007e3b0
 Jetski::Jetski()
 {
-	this->m_unk0x13c = 25.0;
-	this->m_unk0x150 = 2.0;
-	this->m_unk0x148 = 1;
+	m_unk0x13c = 25.0;
+	m_unk0x150 = 2.0;
+	m_unk0x148 = 1;
 }
 
 // STUB: LEGO1 0x1007e630
