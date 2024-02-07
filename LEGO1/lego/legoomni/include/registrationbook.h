917485247ba6dcd1db8ba77b4b81ee9ab5a25fe8
Misha <106913236+MishaProductions@users.noreply.github.com>
2024-02-06 19:06:32 -0500

    implement Enable and Create in RegistrationBook (#535)

--- LEGO1/lego/legoomni/include/registrationbook.h
+++ LEGO1/lego/legoomni/include/registrationbook.h
@@ -3,6 +3,8 @@
 
 #include "legoworld.h"
 
+class InfocenterState;
+
 // VTABLE: LEGO1 0x100d9928
 // SIZE 0x2d0
 class RegistrationBook : public LegoWorld {
@@ -41,19 +43,19 @@ private:
 	undefined m_unk0x100[0x68];  // 0x100
 	undefined m_unk0x168[0x118]; // 0x168
 	struct {
-		undefined4 m_unk0x00[3]; // 0x00
-		undefined2 m_unk0x0c;    // 0x0c
-		undefined2 m_unk0x0e;    // 0x0e
-	} m_unk0x280;                // 0x280
-	undefined m_unk0x290[0x28];  // 0x290
-	undefined2 m_unk0x2b8;       // 0x2b8
-	undefined4 m_unk0x2bc;       // 0x2bc
-	undefined m_unk0x2c0;        // 0x2c0
-	undefined m_unk0x2c1;        // 0x2c1
-	undefined m_unk0x2c2[0x02];  // 0x2c2
-	undefined4 m_unk0x2c4;       // 0x2c4
-	undefined4 m_unk0x2c8;       // 0x2c8
-	undefined4 m_unk0x2cc;       // 0x2cc
+		undefined4 m_unk0x00[3];        // 0x00
+		undefined2 m_unk0x0c;           // 0x0c
+		undefined2 m_unk0x0e;           // 0x0e
+	} m_unk0x280;                       // 0x280
+	undefined m_unk0x290[0x28];         // 0x290
+	undefined2 m_unk0x2b8;              // 0x2b8
+	InfocenterState* m_infocenterState; // 0x2bc
+	undefined m_unk0x2c0;               // 0x2c0
+	undefined m_unk0x2c1;               // 0x2c1
+	undefined m_unk0x2c2[0x02];         // 0x2c2
+	undefined4 m_unk0x2c4;              // 0x2c4
+	undefined4 m_unk0x2c8;              // 0x2c8
+	undefined4 m_unk0x2cc;              // 0x2cc
 };
 
 #endif // REGISTRATIONBOOK_H
