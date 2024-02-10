0463d48540870c7b49177d0ff3fbc246992e7dad
Misha <106913236+MishaProductions@users.noreply.github.com>
2024-02-10 13:02:52 -0500

    implement NotifyEntity (#550)
    * implement NotifyEntity
    
    * Fix/match
    
    * Rename
    
    * Fix style
    
    ---------
    
    Co-authored-by: Christian Semmler <mail@csemmler.com>

--- LEGO1/lego/legoomni/src/common/legoutil.cpp
+++ LEGO1/lego/legoomni/src/common/legoutil.cpp
@@ -2,7 +2,9 @@
 
 #include "legoomni.h"
 #include "legoworld.h"
+#include "legoworldlist.h"
 #include "mxdsaction.h"
+#include "mxnotificationmanager.h"
 #include "mxomni.h"
 #include "mxstreamer.h"
 #include "mxtypes.h"
@@ -128,9 +130,35 @@ MxBool CheckIfEntityExists(MxBool p_enable, const char* p_filename, MxS32 p_enti
 	}
 }
 
-// STUB: LEGO1 0x1003e700
+// FUNCTION: LEGO1 0x1003e700
 void NotifyEntity(const char* p_filename, MxS32 p_entityId, LegoEntity* p_sender)
 {
+	MxAtomId atom(p_filename, e_lowerCase2);
+	LegoEntity* entity = FindWorld(atom, p_entityId);
+
+	if (entity == NULL) {
+		LegoWorldListCursor cursor(Lego()->GetWorldList());
+		LegoWorld* world;
+
+		while (cursor.Next(world)) {
+			entity = (LegoEntity*) world->Find(atom, p_entityId);
+
+			if (entity != NULL) {
+				break;
+			}
+		}
+	}
+
+	if (entity != NULL) {
+#ifdef COMPAT_MODE
+		{
+			MxNotificationParam param(c_notificationType0, p_sender);
+			NotificationManager()->Send(entity, &param);
+		}
+#else
+		NotificationManager()->Send(entity, &MxNotificationParam(c_notificationType0, p_sender));
+#endif
+	}
 }
 
 // FUNCTION: LEGO1 0x1003eae0
