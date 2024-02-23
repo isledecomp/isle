b4c4888b47a88b4bb150fd6b1347e43d45711728
Christian Semmler <mail@csemmler.com>
2024-02-23 11:13:50 -0500

    WIP

--- LEGO1/lego/sources/misc/legocontainer.cpp
+++ LEGO1/lego/sources/misc/legocontainer.cpp
@@ -1,5 +1,9 @@
 #include "legocontainer.h"
 
+#include "lego/legoomni/include/legoomni.h"
+#include "lego/legoomni/include/legovideomanager.h"
+#include "tgl/d3drm/impl.h"
+
 DECOMP_SIZE_ASSERT(LegoContainerInfo<LegoTexture>, 0x10);
 // DECOMP_SIZE_ASSERT(LegoContainer<LegoTexture>, 0x18);
 DECOMP_SIZE_ASSERT(LegoTextureContainer, 0x24);
@@ -9,16 +13,116 @@ LegoTextureContainer::~LegoTextureContainer()
 {
 }
 
-// STUB: LEGO1 0x100998e0
-LegoTextureInfo* LegoTextureContainer::Create(undefined* p_und)
+// FUNCTION: LEGO1 0x100998e0
+LegoTextureInfo* LegoTextureContainer::Insert(LegoTextureInfo* p_textureInfo)
 {
+	DDSURFACEDESC desc;
+	DWORD width, height;
+	memset(&desc, 0, sizeof(desc));
+	desc.dwSize = sizeof(desc);
+
+	if (p_textureInfo->m_surface->Lock(NULL, &desc, DDLOCK_SURFACEMEMORYPTR, NULL) == DD_OK) {
+		width = desc.dwWidth;
+		height = desc.dwHeight;
+		p_textureInfo->m_surface->Unlock(desc.lpSurface);
+	}
+
+#ifdef COMPAT_MODE
+	LegoTextureList::iterator it;
+	for (it = m_list.begin(); it != m_list.end(); it++) {
+#else
+	for (LegoTextureList::iterator it = m_list.begin(); it != m_list.end(); it++) {
+#endif
+		if ((*it).second == FALSE) {
+			LegoTextureInfo* textureInfo = (*it).first;
+
+			if (textureInfo->m_texture->AddRef() != 0 && textureInfo->m_texture->Release() == 1) {
+				if (!strcmp(textureInfo->m_name, p_textureInfo->m_name)) {
+					DDSURFACEDESC desc;
+					memset(&desc, 0, sizeof(desc));
+					desc.dwSize = sizeof(desc);
+
+					if (textureInfo->m_surface->Lock(NULL, &desc, DDLOCK_SURFACEMEMORYPTR, NULL) == DD_OK) {
+						BOOL und = FALSE;
+						if (desc.dwWidth == width && desc.dwHeight == height) {
+							und = TRUE;
+						}
+
+						textureInfo->m_surface->Unlock(desc.lpSurface);
+
+						if (und) {
+							(*it).second = TRUE;
+							textureInfo->m_texture->AddRef();
+							return textureInfo;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	LegoTextureInfo* textureInfo = new LegoTextureInfo();
+
+	textureInfo->m_palette = p_textureInfo->m_palette;
+	p_textureInfo->m_palette->Release();
+
+	DDSURFACEDESC newDesc;
+	memset(&newDesc, 0, sizeof(newDesc));
+	newDesc.dwWidth = desc.dwWidth;
+	newDesc.dwHeight = desc.dwHeight;
+	newDesc.dwSize = sizeof(newDesc);
+	newDesc.dwFlags = DDSD_PIXELFORMAT | DDSD_WIDTH | DDSD_HEIGHT | DDSD_CAPS;
+	newDesc.ddsCaps.dwCaps = DDCAPS_OVERLAYCANTCLIP | DDCAPS_OVERLAY;
+	newDesc.ddpfPixelFormat.dwSize = sizeof(desc.ddpfPixelFormat);
+	newDesc.ddpfPixelFormat.dwFlags = DDPF_RGB | DDPF_PALETTEINDEXED8;
+	newDesc.ddpfPixelFormat.dwRGBBitCount = 8;
+
+	if (VideoManager()->GetDirect3D()->DirectDraw()->CreateSurface(&newDesc, &textureInfo->m_surface, NULL) == DD_OK) {
+		RECT rect;
+		rect.left = 0;
+		rect.top = newDesc.dwWidth - 1;
+		rect.right = 0;
+		rect.bottom = newDesc.dwHeight - 1;
+
+		textureInfo->m_surface->SetPalette(textureInfo->m_palette);
+
+		if (textureInfo->m_surface->BltFast(0, 0, p_textureInfo->m_surface, &rect, DDBLTFAST_WAIT) != DD_OK) {
+			textureInfo->m_surface->Release();
+			textureInfo->m_palette->Release();
+			delete textureInfo;
+			textureInfo = NULL;
+		}
+		else {
+			if (((TglImpl::RendererImpl*) VideoManager()->GetRenderer())
+					->CreateTextureFromSurface(textureInfo->m_surface, &textureInfo->m_texture) != D3DRM_OK) {
+				textureInfo->m_surface->Release();
+				textureInfo->m_palette->Release();
+				delete textureInfo;
+				textureInfo = NULL;
+			}
+			else {
+				textureInfo->m_texture->SetAppData((DWORD) textureInfo);
+				m_list.push_back(LegoTextureListElement(textureInfo, TRUE));
+
+				if (textureInfo->m_name != NULL) {
+					delete[] textureInfo->m_name;
+				}
+
+				textureInfo->m_name = new char[strlen(p_textureInfo->m_name) + 1];
+				strcpy(textureInfo->m_name, p_textureInfo->m_name);
+
+				return textureInfo;
+			}
+		}
+	}
+
 	return NULL;
 }
 
 // FUNCTION: LEGO1 0x10099cc0
-void LegoTextureContainer::Destroy(LegoTextureInfo* p_data)
+void LegoTextureContainer::Erase(LegoTextureInfo* p_textureInfo)
 {
-	if (p_data == NULL) {
+	if (p_textureInfo == NULL) {
 		return;
 	}
 
@@ -28,12 +132,11 @@ void LegoTextureContainer::Destroy(LegoTextureInfo* p_data)
 #else
 	for (LegoTextureList::iterator it = m_list.begin(); it != m_list.end(); it++) {
 #endif
-		if (((*it).first) == p_data) {
-			// TODO: Element type
-			(*it).second = 0;
+		if ((*it).first == p_textureInfo) {
+			(*it).second = FALSE;
 
-			if (p_data->m_texture->Release() == TRUE) {
-				delete p_data;
+			if (p_textureInfo->m_texture->Release() == TRUE) {
+				delete p_textureInfo;
 				m_list.erase(it);
 			}
 
