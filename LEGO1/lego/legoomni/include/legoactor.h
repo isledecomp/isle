51520aaae6827cba51a316fd4ec94b6eed26938c
Joshua Peisach <itzswirlz2020@outlook.com>
2024-02-23 12:51:49 -0500

    LegoActor: destructor, SetROI (#588)
    * LegoActor: destructor, SetROI
    
    * Fixes
    
    ---------
    
    Co-authored-by: Christian Semmler <mail@csemmler.com>

--- LEGO1/lego/legoomni/include/legoactor.h
+++ LEGO1/lego/legoomni/include/legoactor.h
@@ -2,6 +2,7 @@
 #define LEGOACTOR_H
 
 #include "decomp.h"
+#include "legocachesound.h"
 #include "legoentity.h"
 
 // VTABLE: LEGO1 0x100d6d68
@@ -29,22 +30,27 @@ public:
 
 	// FUNCTION: LEGO1 0x10002cc0
 	virtual MxFloat VTable0x50() { return m_unk0x68; } // vtable+0x50
+
 	// FUNCTION: LEGO1 0x10002cd0
 	virtual void VTable0x54(MxFloat p_unk0x68) { m_unk0x68 = p_unk0x68; } // vtable+0x54
+
 	// FUNCTION: LEGO1 0x10002ce0
 	virtual void VTable0x58(MxFloat p_unk0x70) { m_unk0x70 = p_unk0x70; } // vtable+0x58
+
 	// FUNCTION: LEGO1 0x10002cf0
 	virtual MxFloat VTable0x5c() { return m_unk0x70; } // vtable+0x5c
+
 	// FUNCTION: LEGO1 0x10002d00
 	virtual MxU8 VTable0x60() { return m_unk0x74; } // vtable+0x60
+
 	// FUNCTION: LEGO1 0x10002d10
 	virtual void VTable0x64(MxU8 p_unk0x74) { m_unk0x74 = p_unk0x74; } // vtable+0x64
 
 private:
-	MxFloat m_unk0x68;    // 0x68
-	undefined4 m_unk0x6c; // 0x6c
-	MxFloat m_unk0x70;    // 0x70
-	MxU8 m_unk0x74;       // 0x74
+	MxFloat m_unk0x68;       // 0x68
+	LegoCacheSound* m_sound; // 0x6c
+	MxFloat m_unk0x70;       // 0x70
+	MxU8 m_unk0x74;          // 0x74
 };
 
 // SYNTHETIC: LEGO1 0x1002d300
