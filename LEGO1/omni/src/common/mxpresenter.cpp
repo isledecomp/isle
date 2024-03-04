068760056afd6792949c487709e663cc686d7f5e
Misha <106913236+MishaProductions@users.noreply.github.com>
2024-03-04 17:00:38 -0500

    fix bug in MxPresenter::CreateEntity (#626)
    * fix bug in MxPresenter::CreateEntity
    
    * Match 100%
    
    ---------
    
    Co-authored-by: Christian Semmler <mail@csemmler.com>

--- LEGO1/omni/src/common/mxpresenter.cpp
+++ LEGO1/omni/src/common/mxpresenter.cpp
@@ -239,24 +239,26 @@ const char* PresenterNameDispatch(const MxDSAction& p_action)
 }
 
 // FUNCTION: LEGO1 0x100b5410
-MxEntity* MxPresenter::CreateEntity(const char* p_name)
+MxEntity* MxPresenter::CreateEntity(const char* p_defaultName)
 {
-	char buffer[512];
-	char buffer2[512];
-	strcpy(buffer, p_name);
+	// create an object from LegoObjectFactory based on OBJECT: value in extra data.
+	// If that is missing, p_defaultName is used
 
-	MxU16 extraLen = m_action->GetExtraLength();
+	char objectName[512];
+	strcpy(objectName, p_defaultName);
 
-	buffer[0] = extraLen;
-	buffer[1] = extraLen >> 8;
-	if (extraLen) {
-		extraLen &= MAXWORD;
-		memcpy(buffer2 + 2, m_action->GetExtraData(), extraLen);
-		buffer2[extraLen + 2] = 0;
-		KeyValueStringParse(buffer, g_strOBJECT, buffer2 + 2);
+	MxU16 extraLength;
+	char* extraData;
+	m_action->GetExtra(extraLength, extraData);
+
+	if (extraLength & MAXWORD) {
+		char extraCopy[512];
+		memcpy(extraCopy, extraData, extraLength & MAXWORD);
+		extraCopy[extraLength & MAXWORD] = '\0';
+		KeyValueStringParse(objectName, g_strOBJECT, extraCopy);
 	}
 
-	return (MxEntity*) ObjectFactory()->Create(buffer);
+	return (MxEntity*) ObjectFactory()->Create(objectName);
 }
 
 // FUNCTION: LEGO1 0x100b54c0
