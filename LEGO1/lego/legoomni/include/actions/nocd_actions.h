b57d640f25fd0d7d8c3ebd0643a66ce8d2a0d10a
Ramen2X <rmn@legoisland.org>
2024-03-10 10:34:43 -0400

    scope headers in namespaces instead

--- LEGO1/lego/legoomni/include/actions/nocd_actions.h
+++ LEGO1/lego/legoomni/include/actions/nocd_actions.h
@@ -3,15 +3,15 @@
 #ifndef NOCD_ACTIONS_H
 #define NOCD_ACTIONS_H
 
-class NocdScript {
-public:
-	enum Script {
-		c_noneNocd = -1,
+namespace NocdScript
+{
+enum Script {
+	c_noneNocd = -1,
 
-		c_NoCD_Movie = 0,
-		c_CDSpin1_Smk = 1,
-		c_Iicx62In_Wave = 2
-	};
+	c_NoCD_Movie = 0,
+	c_CDSpin1_Smk = 1,
+	c_Iicx62In_Wave = 2
 };
+} // namespace NocdScript
 
 #endif // NOCD_ACTIONS_H
