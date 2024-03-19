6fffce5c5921367e974b6eb38cc6aa213f4c45f6
Christian Semmler <mail@csemmler.com>
2024-03-19 13:38:10 -0400

    Add TODO

--- LEGO1/lego/legoomni/src/common/legocharactermanager.cpp
+++ LEGO1/lego/legoomni/src/common/legocharactermanager.cpp
@@ -133,6 +133,7 @@ LegoROI* LegoCharacterManager::GetROI(const char* p_key, MxBool p_createEntity)
 
 	if (character != NULL) {
 		if (p_createEntity && character->m_roi->GetEntity() == NULL) {
+			// TODO: Match
 			LegoAnimActor* actor = new LegoAnimActor(1);
 
 			actor->SetROI(character->m_roi, FALSE, FALSE);
