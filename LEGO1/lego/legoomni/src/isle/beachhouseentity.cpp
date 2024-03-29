e9a18fd476e9e21a6610c074e654f3d8b6a42a35
Joshua Peisach <itzswirlz2020@outlook.com>
2024-03-29 13:51:38 -0400

    Run clang-format

--- LEGO1/lego/legoomni/src/isle/beachhouseentity.cpp
+++ LEGO1/lego/legoomni/src/isle/beachhouseentity.cpp
@@ -17,20 +17,20 @@ DECOMP_SIZE_ASSERT(BeachHouseEntity, 0x68)
 // FUNCTION: LEGO1 0x100153b0
 MxLong BeachHouseEntity::VTable0x50(MxParam& p_param)
 {
-    if (FUN_1003ef60()) {
-        Act1State* state = (Act1State*) GameState()->GetState("Act1State");
-        state->SetUnknown18(0);
+	if (FUN_1003ef60()) {
+		Act1State* state = (Act1State*) GameState()->GetState("Act1State");
+		state->SetUnknown18(0);
 
-        if (CurrentActor()->GetActorId() != GameState()->GetActorId()) {
+		if (CurrentActor()->GetActorId() != GameState()->GetActorId()) {
 			CurrentActor()->VTable0xe4();
 		}
 
-    	Isle* isle = (Isle*) FindWorld(*g_isleScript, IsleScript::c__Isle);
+		Isle* isle = (Isle*) FindWorld(*g_isleScript, IsleScript::c__Isle);
 		isle->SetDestLocation(LegoGameState::Area::e_jetskibuild);
 
-        AnimationManager()->FUN_10061010(NULL);
+		AnimationManager()->FUN_10061010(NULL);
 		TransitionManager()->StartTransition(MxTransitionManager::e_mosaic, 50, FALSE, FALSE);
-    }
+	}
 
-    return 1;
+	return 1;
 }
