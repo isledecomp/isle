1bfe47357b93c28084fc55c500706f6ed237f93f
Christian Semmler <mail@csemmler.com>
2024-04-07 08:36:58 -0400

    Refactor LegoActorStruct into unknown class in misc lib (#782)

--- LEGO1/lego/legoomni/src/paths/legopathactor.cpp
+++ LEGO1/lego/legoomni/src/paths/legopathactor.cpp
@@ -124,28 +124,3 @@ void LegoPathActor::VTable0xa8()
 {
 	// TODO
 }
-
-// FUNCTION: LEGO1 0x1009a0f0
-LegoActorStruct::LegoActorStruct()
-{
-	for (MxS32 i = 0; i < _countof(m_unk0x00); i++) {
-		m_unk0x00[i].Clear();
-	}
-}
-
-// FUNCTION: LEGO1 0x1009a130
-LegoActorStruct::~LegoActorStruct()
-{
-}
-
-// FUNCTION: LEGO1 0x1009a140
-void LegoActorStruct::FUN_1009a140(Vector3& p_point1, Vector3& p_point2, Vector3& p_point3, Vector3& p_point4)
-{
-	m_unk0x00[0] = p_point1;
-	m_unk0x00[1] = p_point2;
-
-	for (MxS32 i = 0; i < 3; i++) {
-		m_unk0x00[2][i] = (p_point3[i] - p_point1[i]) * 3.0f - p_point2[i] * 2.0f - p_point4[i];
-		m_unk0x00[3][i] = (p_point1[i] - p_point3[i]) * 2.0f + p_point4[i] + p_point2[i];
-	}
-}
