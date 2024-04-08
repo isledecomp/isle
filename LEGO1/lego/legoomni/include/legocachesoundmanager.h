87f1e2144a1c4ec468eadb70a1df8d63ccecff3c
Christian Semmler <mail@csemmler.com>
2024-04-08 08:12:57 -0400

    Implement/match LegoActor::ParseAction

--- LEGO1/lego/legoomni/include/legocachesoundmanager.h
+++ LEGO1/lego/legoomni/include/legocachesoundmanager.h
@@ -54,7 +54,7 @@ public:
 
 	LegoCacheSound* FUN_1003d170(const char* p_key);
 	LegoCacheSound* FUN_1003d290(LegoCacheSound* p_sound);
-	void FUN_1003dae0(const char* p_one, const char* p_two, MxBool p_three);
+	LegoCacheSound* FUN_1003dae0(const char* p_one, const char* p_two, MxBool p_three);
 	LegoCacheSound* FUN_1003db10(LegoCacheSound* p_one, const char* p_two, MxBool p_three);
 	void FUN_1003dc40(LegoCacheSound** p_und);
 
