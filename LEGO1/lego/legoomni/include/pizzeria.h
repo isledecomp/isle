4912bc61fb2b2e5992e64f7732c3ec3f9256e6e8
Joshua Peisach <itzswirlz2020@outlook.com>
2024-04-10 09:27:49 -0400

    clang-format

--- LEGO1/lego/legoomni/include/pizzeria.h
+++ LEGO1/lego/legoomni/include/pizzeria.h
@@ -8,31 +8,31 @@
 // SIZE 0x84
 class Pizzeria : public IsleActor {
 public:
-  Pizzeria() : m_unk0x7c(0) {}
+	Pizzeria() : m_unk0x7c(0) {}
 
-  // FUNCTION: LEGO1 0x1000e780
-  inline const char *ClassName() const override // vtable+0x0c
-  {
-    // STRING: LEGO1 0x100f0380
-    return "Pizzeria";
-  }
+	// FUNCTION: LEGO1 0x1000e780
+	inline const char* ClassName() const override // vtable+0x0c
+	{
+		// STRING: LEGO1 0x100f0380
+		return "Pizzeria";
+	}
 
-  // FUNCTION: LEGO1 0x1000e790
-  inline MxBool IsA(const char *p_name) const override // vtable+0x10
-  {
-    return !strcmp(p_name, Pizzeria::ClassName()) || IsleActor::IsA(p_name);
-  }
+	// FUNCTION: LEGO1 0x1000e790
+	inline MxBool IsA(const char* p_name) const override // vtable+0x10
+	{
+		return !strcmp(p_name, Pizzeria::ClassName()) || IsleActor::IsA(p_name);
+	}
 
-  MxResult Create(MxDSAction &p_dsAction) override; // vtable+0x18
+	MxResult Create(MxDSAction& p_dsAction) override; // vtable+0x18
 
-  virtual undefined4 VTable0x68() override; // vtable+0x68
+	virtual undefined4 VTable0x68() override; // vtable+0x68
 
-  // SYNTHETIC: LEGO1 0x1000e8d0
-  // Pizzeria::`scalar deleting destructor'
+	// SYNTHETIC: LEGO1 0x1000e8d0
+	// Pizzeria::`scalar deleting destructor'
 
 private:
-  undefined4 m_unk0x7c; // 0x7c
-  undefined4 m_unk0x80; // 0x80
+	undefined4 m_unk0x7c; // 0x7c
+	undefined4 m_unk0x80; // 0x80
 };
 
 #endif // PIZZERIA_H
