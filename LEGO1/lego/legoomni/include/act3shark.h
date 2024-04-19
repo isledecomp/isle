f10966e3f3ef660cd60f5ccbaa1dc6f46af1b527
disinvite <disinvite@users.noreply.github.com>
2024-04-19 16:03:52 -0400

    Enable some multiple-inheritance vtables

--- LEGO1/lego/legoomni/include/act3shark.h
+++ LEGO1/lego/legoomni/include/act3shark.h
@@ -3,36 +3,27 @@
 
 #include "legoanimactor.h"
 
-/*
-	VTABLE: LEGO1 0x100d7920 LegoPathActor
-	VTABLE: LEGO1 0x100d79f0 LegoAnimActor
-*/
+// VTABLE: LEGO1 0x100d7920 LegoPathActor
+// VTABLE: LEGO1 0x100d79f0 LegoAnimActor
 // SIZE 0x1a8
 class Act3Shark : public LegoAnimActor {
 public:
 	Act3Shark();
 
-	// FUNCTION: LEGO1 0x100430c0
+	// FUNCTION: LEGO1 0x100430d0
 	inline const char* ClassName() const override // vtable+0x0c
 	{
 		// STRING: LEGO1 0x100f03a0
 		return "Act3Shark";
 	}
 
-	// FUNCTION: LEGO1 0x1001a130
-	inline MxBool IsA(const char* p_name) const override // vtable+0x10
-	{
-		return !strcmp(p_name, Act3Shark::ClassName()) || LegoAnimActor::IsA(p_name);
-	}
+	void ParseAction(char*) override;        // vtable+0x20
+	void VTable0x70(float p_float) override; // vtable+0x70
 
-	void ParseAction(char*) override;                  // vtable+0x20
-	void SetWorldSpeed(MxFloat p_worldSpeed) override; // vtable+0x30
-	void VTable0x70(float p_float) override;           // vtable+0x70
-	void VTable0x74(Matrix4& p_transform) override;    // vtable+0x74
+	// LegoAnimActor vtable
+	virtual MxResult FUN_10042ce0(void*); // vtable+0x10
 
-	virtual MxResult FUN_10042ce0(void*);
-
-	// SYNTHETIC: LEGO1 0x10043020
+	// SYNTHETIC: LEGO1 0x10043030
 	// Act3Shark::`scalar deleting destructor'
 
 private:
@@ -42,5 +33,10 @@ private:
 	undefined m_unk0x30[0x0c]; // 0x30
 	Mx3DPointFloat m_unk0x3c;  // 0x3c
 };
+
+// STUB: LEGO1 0x10042c90
+// List<void *>::~List<void *>
+// TODO: Update once type is known.
+// STUB to resolve diff in scalar dtor and not create a new one.
 
 #endif // ACT3SHARK_H
--- LEGO1/lego/legoomni/src/actors/act2actor.cpp
+++ LEGO1/lego/legoomni/src/actors/act2actor.cpp
@@ -7,3 +7,49 @@ Act2Actor::Act2Actor()
 {
 	// TODO
 }
+
+// STUB: LEGO1 0x10018940
+void Act2Actor::SetROI(LegoROI* p_roi, MxBool p_bool1, MxBool p_bool2)
+{
+	// TODO
+}
+
+// STUB: LEGO1 0x100189f0
+MxResult Act2Actor::VTable0x94(LegoPathActor*, MxBool)
+{
+	// TODO
+	return SUCCESS;
+}
+
+// STUB: LEGO1 0x10018a20
+MxResult Act2Actor::WaitForAnimation()
+{
+	// TODO
+	return SUCCESS;
+}
+
+// STUB: LEGO1 0x10018c30
+void Act2Actor::VTable0x70(float p_und)
+{
+	// TODO
+}
+
+// STUB: LEGO1 0x10019280
+void Act2Actor::SetWorldSpeed(MxFloat p_worldSpeed)
+{
+	// TODO
+}
+
+// STUB: LEGO1 0x100195a0
+MxS32 Act2Actor::VTable0xa0()
+{
+	// TODO
+	return 0;
+}
+
+// STUB: LEGO1 0x1001a180
+MxS32 Act2Actor::VTable0x68(Vector3&, Vector3&, Vector3&)
+{
+	// TODO
+	return 0;
+}
