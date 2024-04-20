c617139d5b4c576b05578623c608a6f961c86b26
Christian Semmler <mail@csemmler.com>
2024-04-20 10:41:34 -0400

    Implement/match LegoAnimMMPresenter::EndAction (#826)
    * Implement/match LegoAnimMMPresenter::EndAction
    
    * Add missing vtable

--- LEGO1/omni/include/mxtype18notificationparam.h
+++ LEGO1/omni/include/mxtype18notificationparam.h
@@ -7,8 +7,26 @@
 // VTABLE: LEGO1 0x100d7e80
 // SIZE 0x10
 class MxType18NotificationParam : public MxNotificationParam {
+public:
+	MxType18NotificationParam(NotificationId p_type, MxCore* p_sender, undefined4 p_unk0x0c)
+		: MxNotificationParam(p_type, p_sender), m_unk0x0c(p_unk0x0c)
+	{
+	}
+
+	// FUNCTION: LEGO1 0x1004afd0
+	MxNotificationParam* Clone() override
+	{
+		return new MxType18NotificationParam(m_type, m_sender, m_unk0x0c);
+	} // vtable+0x04
+
 protected:
 	undefined4 m_unk0x0c; // 0x0c
 };
+
+// SYNTHETIC: LEGO1 0x1004b080
+// MxType18NotificationParam::`scalar deleting destructor'
+
+// SYNTHETIC: LEGO1 0x1004b0f0
+// MxType18NotificationParam::~MxType18NotificationParam
 
 #endif // MXTYPE18NOTIFICATIONPARAM_H
