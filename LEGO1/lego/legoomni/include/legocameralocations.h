39afca73f32e965aa414707fec9f3259c0409a45
Christian Semmler <mail@csemmler.com>
2024-05-10 10:17:49 -0400

    Fix member name

--- LEGO1/lego/legoomni/include/legocameralocations.h
+++ LEGO1/lego/legoomni/include/legocameralocations.h
@@ -8,12 +8,12 @@
 struct LegoCameraLocation {
 	// SIZE 0x18
 	struct Path {
-		const char* m_name;     // 0x00
-		MxS32 m_src;            // 0x04
-		float m_srcScale;       // 0x08
-		MxS32 m_dest;           // 0x0c
-		float m_destScale;      // 0x10
-		undefined4 m_unk0x0x10; // 0x14
+		const char* m_name;   // 0x00
+		MxS32 m_src;          // 0x04
+		float m_srcScale;     // 0x08
+		MxS32 m_dest;         // 0x0c
+		float m_destScale;    // 0x10
+		undefined4 m_unk0x10; // 0x14
 	};
 
 	MxU32 m_index;        // 0x00
