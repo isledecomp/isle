58c910559649f6662cd9a6b15b498f75d4035fad
Christian Semmler <mail@csemmler.com>
2024-05-14 10:35:00 -0400

    Move strings to proper location in define.cpp

--- LEGO1/define.h
+++ LEGO1/define.h
@@ -18,6 +18,21 @@ extern const char* g_strFROM_PARENT;
 extern const char* g_strHIDE_ON_STOP;
 extern const char* g_strMUST_SUCCEED;
 extern const char* g_strSUBST;
+extern const char* g_strTRIGGERS_SOURCE;
 extern const char* g_strPTATCAM;
+extern const char* g_strTOGGLE;
+extern const char* g_strMAP;
+extern const char* g_strGRID;
+extern const char* g_strSTYLE;
+extern const char* g_strTYPE;
+extern const char* g_strLEFT_TO_RIGHT;
+extern const char* g_strRIGHT_TO_LEFT;
+extern const char* g_strBOTTOM_TO_TOP;
+extern const char* g_strTOP_TO_BOTTOM;
+extern const char* g_strFILLER_INDEX;
+extern const char* g_strVARIABLE;
+extern const char* g_strBMP_ISMAP;
+extern const char* g_strAUTO_CREATE;
+extern const char* g_strDB_CREATE;
 
 #endif // DEFINE_H
