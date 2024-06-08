881961ae787f2824e15db42c02136405416fcd9f
Joshua Peisach <itzswirlz2020@outlook.com>
2024-06-08 11:20:37 -0400

    Act2PoliceStation::Notify

--- LEGO1/lego/legoomni/src/entity/act2policestation.cpp
+++ LEGO1/lego/legoomni/src/entity/act2policestation.cpp
@@ -1,11 +1,20 @@
 #include "act2policestation.h"
 
+#include "legoworld.h"
+#include "misc.h"
+#include "mxmisc.h"
+#include "mxnotificationmanager.h"
+#include "mxnotificationparam.h"
+
 DECOMP_SIZE_ASSERT(Act2PoliceStation, 0x68)
 
-// STUB: LEGO1 0x1004e0e0
+// FUNCTION: LEGO1 0x1004e0e0
 MxLong Act2PoliceStation::Notify(MxParam& p_param)
 {
-	// TODO
+	if (((MxNotificationParam&) p_param).GetType() == c_notificationClick) {
+		NotificationManager()->Send(CurrentWorld(), MxNotificationParam(c_notificationType23, NULL));
+		return 1;
+	}
 
-	return 0;
+	return -1;
 }
