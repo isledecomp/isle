a226f5d59ec406be665925b409db9fe45c89b44a
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-06-25 15:02:47 +0200

    Fix warning about assigning const string literals to variable char pointers

--- LEGO1/lego/legoomni/include/legoanimationmanager.h
+++ LEGO1/lego/legoomni/include/legoanimationmanager.h
@@ -96,7 +96,7 @@ class LegoAnimationManager : public MxCore {
 public:
 	// SIZE 0x18
 	struct Character {
-		char* m_name;        // 0x00
+		const char* m_name;  // 0x00
 		MxBool m_inExtras;   // 0x04
 		MxS8 m_vehicleId;    // 0x05
 		undefined m_unk0x06; // 0x06 (unused?)
@@ -112,9 +112,9 @@ public:
 
 	// SIZE 0x08
 	struct Vehicle {
-		char* m_name;     // 0x00
-		MxBool m_unk0x04; // 0x04
-		MxBool m_unk0x05; // 0x05
+		const char* m_name; // 0x00
+		MxBool m_unk0x04;   // 0x04
+		MxBool m_unk0x05;   // 0x05
 	};
 
 	// SIZE 0x18
