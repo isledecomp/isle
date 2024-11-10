7baa9b49170739c46ef3569b1e1181d7941db250
Christian Semmler <mail@csemmler.com>
2024-11-10 08:19:33 -0700

    Implement/match Act2GenActor::VTable0x94

--- LEGO1/lego/legoomni/src/actors/act2genactor.cpp
+++ LEGO1/lego/legoomni/src/actors/act2genactor.cpp
@@ -1,10 +1,32 @@
 #include "act2genactor.h"
 
+#include "legocachesoundmanager.h"
+#include "legosoundmanager.h"
+#include "misc.h"
+#include "mxmisc.h"
+#include "mxtimer.h"
+#include "roi/legoroi.h"
+
 DECOMP_SIZE_ASSERT(Act2GenActor, 0x154)
 
-// STUB: LEGO1 0x10018740
-MxResult Act2GenActor::VTable0x94(LegoPathActor*, MxBool)
+// GLOBAL: LEGO1 0x100f0f18
+MxLong Act2GenActor::g_lastHitActorTime = 0;
+
+// FUNCTION: LEGO1 0x10018740
+// FUNCTION: BETA10 0x1000c7a0
+MxResult Act2GenActor::VTable0x94(LegoPathActor* p_actor, MxBool)
 {
-	// TODO
+	MxLong time = Timer()->GetTime();
+	MxLong diff = time - g_lastHitActorTime;
+
+	if (strcmp(p_actor->GetROI()->GetName(), "pepper")) {
+		return SUCCESS;
+	}
+
+	g_lastHitActorTime = time;
+	if (diff > 1000) {
+		SoundManager()->GetCacheSoundManager()->Play("hitactor", NULL, FALSE);
+	}
+
 	return SUCCESS;
 }
