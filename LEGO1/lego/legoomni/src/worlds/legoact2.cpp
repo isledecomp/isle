71a1c515968163b5a3c9dae5d4d55fc3272c7f45
Christian Semmler <mail@csemmler.com>
2024-11-11 11:30:34 -0700

    Implement/match LegoAct2::Escape (#1147)
    * Implement/match LegoAct2::Escape
    
    * Match empty function

--- LEGO1/lego/legoomni/src/worlds/legoact2.cpp
+++ LEGO1/lego/legoomni/src/worlds/legoact2.cpp
@@ -1,9 +1,12 @@
 #include "legoact2.h"
 
+#include "act2main_actions.h"
+#include "islepathactor.h"
 #include "legoanimationmanager.h"
 #include "legogamestate.h"
 #include "legoinputmanager.h"
 #include "misc.h"
+#include "mxbackgroundaudiomanager.h"
 #include "mxmisc.h"
 #include "mxnotificationmanager.h"
 #include "mxticklemanager.h"
@@ -127,15 +130,29 @@ void LegoAct2::FUN_10051900()
 	}
 }
 
-// STUB: LEGO1 0x100519c0
+// FUNCTION: LEGO1 0x100519c0
 void LegoAct2::VTable0x60()
 {
-	// TODO
+	// empty
 }
 
-// STUB: LEGO1 0x100519d0
+// FUNCTION: LEGO1 0x100519d0
 MxBool LegoAct2::Escape()
 {
-	// TODO
-	return FALSE;
+	BackgroundAudioManager()->Stop();
+	AnimationManager()->FUN_10061010(FALSE);
+	DeleteObjects(&m_atomId, Act2mainScript::c_snsx50bu_RunAnim, 999);
+
+	if (UserActor() != NULL) {
+		if (UserActor()->GetActorId() != GameState()->GetActorId()) {
+			((IslePathActor*) UserActor())->Exit();
+		}
+	}
+
+	if (m_gameState != NULL) {
+		m_gameState->m_unk0x0c = 0;
+	}
+
+	m_unk0x1150 = 2;
+	return TRUE;
 }
