f301b6b1898600a6a8a5630f9b8f928bdf2098c0
disinvite <disinvite@users.noreply.github.com>
2024-12-22 21:41:00 -0500

    MxDisplaySurface::VTable0x24

--- LEGO1/omni/include/mxdisplaysurface.h
+++ LEGO1/omni/include/mxdisplaysurface.h
@@ -27,14 +27,14 @@ public:
 	virtual void Destroy();                              // vtable+0x1c
 	virtual void SetPalette(MxPalette* p_palette);       // vtable+0x20
 	virtual void VTable0x24(
-		LPDDSURFACEDESC,
-		MxBitmap*,
-		undefined4,
-		undefined4,
-		undefined4,
-		undefined4,
-		undefined4,
-		undefined4
+		LPDDSURFACEDESC p_desc,
+		MxBitmap* p_bitmap,
+		MxS32 p_left,
+		MxS32 p_top,
+		MxS32 p_right,
+		MxS32 p_bottom,
+		MxS32 p_width,
+		MxS32 p_height
 	); // vtable+0x24
 	virtual void VTable0x28(
 		MxBitmap* p_bitmap,
