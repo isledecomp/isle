49cb12079532f3443b17980968a06e6b0c818c88
Christian Semmler <mail@csemmler.com>
2025-01-22 14:22:58 -0700

    Order symbols until end of `LegoOmni` (#1371)
    * Order
    
    * Improve
    
    * Move function
    
    * Order until end of LegoOmni
    
    * Remove header

--- LEGO1/lego/legoomni/include/legoracespecial.h
+++ LEGO1/lego/legoomni/include/legoracespecial.h
@@ -35,7 +35,7 @@ public:
 		return !strcmp(p_name, LegoCarRaceActor::ClassName()) || LegoRaceActor::IsA(p_name);
 	}
 
-	MxU32 VTable0x6c(
+	inline MxU32 VTable0x6c(
 		LegoPathBoundary* p_boundary,
 		Vector3& p_v1,
 		Vector3& p_v2,
@@ -99,6 +99,55 @@ protected:
 	static MxFloat g_unk0x100f7aec;
 };
 
+// VTABLE: LEGO1 0x100da208 LegoCarRaceActor
+// VTABLE: LEGO1 0x100da228 LegoRaceActor
+// VTABLE: LEGO1 0x100da230 LegoAnimActor
+// VTABLE: LEGO1 0x100da240 LegoPathActor
+// VTABLE: BETA10 0x101bd348 LegoCarRaceActor
+// VTABLE: BETA10 0x101bd370 LegoRaceActor
+// VTABLE: BETA10 0x101bd378 LegoAnimActor
+// VTABLE: BETA10 0x101bd390 LegoPathActor
+// SIZE 0x1a8
+class LegoJetskiRaceActor : public virtual LegoCarRaceActor {
+public:
+	LegoJetskiRaceActor();
+
+	// FUNCTION: LEGO1 0x10081d90
+	// FUNCTION: BETA10 0x100aa920
+	const char* ClassName() const override // vtable+0x0c
+	{
+		// STRING: LEGO1 0x100f0554
+		return "LegoJetskiRaceActor";
+	}
+
+	// FUNCTION: LEGO1 0x10081db0
+	// FUNCTION: BETA10 0x100aa960
+	MxBool IsA(const char* p_name) const override // vtable+0x10
+	{
+		return !strcmp(p_name, LegoJetskiRaceActor::ClassName()) || LegoCarRaceActor::IsA(p_name);
+	}
+
+	inline MxU32 VTable0x6c(
+		LegoPathBoundary* p_boundary,
+		Vector3& p_v1,
+		Vector3& p_v2,
+		float p_f1,
+		float p_f2,
+		Vector3& p_v3
+	) override;                                                                // vtable+0x6c
+	void Animate(float p_time) override;                                       // vtable+0x70
+	MxS32 VTable0x1c(LegoPathBoundary* p_boundary, LegoEdge* p_edge) override; // vtable+0x1c
+
+	// SYNTHETIC: LEGO1 0x10013a80
+	// LegoJetskiRaceActor::`vbase destructor'
+
+	// SYNTHETIC: LEGO1 0x10081d50
+	// LegoJetskiRaceActor::`scalar deleting destructor'
+
+	// SYNTHETIC: LEGO1 0x10013ba0
+	// LegoJetskiRaceActor::~LegoJetskiRaceActor
+};
+
 // GLOBAL: LEGO1 0x100da0b0
 // LegoCarRaceActor::`vbtable'
 
@@ -107,5 +156,17 @@ protected:
 
 // GLOBAL: LEGO1 0x100da098
 // LegoCarRaceActor::`vbtable'{for `LegoRaceActor'}
+
+// GLOBAL: LEGO1 0x100da1f0
+// LegoJetskiRaceActor::`vbtable'{for `LegoJetskiRaceActor'}
+
+// GLOBAL: LEGO1 0x100da1e8
+// LegoJetskiRaceActor::`vbtable'{for `LegoAnimActor'}
+
+// GLOBAL: LEGO1 0x100da1d8
+// LegoJetskiRaceActor::`vbtable'{for `LegoRaceActor'}
+
+// GLOBAL: LEGO1 0x100da1c8
+// LegoJetskiRaceActor::`vbtable'{for `LegoCarRaceActor'}
 
 #endif // LEGOCARRACEACTOR_H
