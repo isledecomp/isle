8acfe39e4c654f8a2dbb2179b3da7ded3fc396df
Tom van Dijk <18gatenmaker6@gmail.com>
2025-05-12 20:44:58 +0200

    feat(docker): Use parallel compilation

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,17 +3,20 @@
 # Populate the Windows path inside of the wineprefix
 # TODO: This is in here because writing to the registry seems
 # to fail when performed in the Dockerfile itself; investigate
-wine reg ADD 'HKCU\Environment' /v PATH /d 'C:\msvc\bin;C:\msvc\bin\winnt;C:\cmake\bin;C:\windows\system32' /f
+wine reg ADD 'HKCU\Environment' /v PATH /d 'C:\msvc\bin;C:\msvc\bin\winnt;C:\cmake\bin;C:\windows\system32;C:\' /f
 wine reg ADD 'HKCU\Environment' /v INCLUDE /d 'C:\msvc\include;C:\msvc\mfc\include' /f
 wine reg ADD 'HKCU\Environment' /v LIB /d 'C:\msvc\lib;C:\msvc\mfc\lib' /f
 wine reg ADD 'HKCU\Environment' /v TMP /d 'Z:\build' /f
 wine reg ADD 'HKCU\Environment' /v TEMP /d 'Z:\build' /f
 
 # Configure build with CMake
-wine cmake -B build isle -G "NMake Makefiles" $CMAKE_FLAGS
+wine cmake -B build isle -G "Ninja" $CMAKE_FLAGS
 
 # Start compiling LEGO Island
-wine cmake --build build
+if [ "x$JOBS" = "x" ]; then
+    JOBS=$(nproc)
+fi
+wine cmake --build build --parallel $JOBS
 
 # Unlock directories
 chmod -R 777 isle build
