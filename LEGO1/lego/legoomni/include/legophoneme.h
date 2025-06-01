104bcdb795502408b7f3af56d9c46703639713d0
Fabian Neundorf <CommodoreFabianus@gmx.de>
2025-06-02 01:00:38 +0200

    Clear unknowns in `LegoPhoneme`

--- LEGO1/lego/legoomni/include/legophoneme.h
+++ LEGO1/lego/legoomni/include/legophoneme.h
@@ -10,32 +10,32 @@ class LegoTextureInfo;
 // SIZE 0x20
 class LegoPhoneme {
 public:
-	LegoPhoneme(const char* p_name, undefined4 p_unk0x14)
+	LegoPhoneme(const char* p_name, MxU32 p_count)
 	{
 		m_name = p_name;
 		m_name.ToUpperCase();
 		Init();
-		m_unk0x14 = p_unk0x14;
+		m_count = p_count;
 	}
 	~LegoPhoneme();
 
-	virtual undefined4 VTable0x00();                     // vtable+0x00
-	virtual void VTable0x04(undefined4 p_unk0x14);       // vtable+0x04
-	virtual LegoTextureInfo* VTable0x08();               // vtable+0x08
-	virtual void VTable0x0c(LegoTextureInfo* p_unk0x18); // vtable+0x0c
-	virtual LegoTextureInfo* VTable0x10();               // vtable+0x10
-	virtual void VTable0x14(LegoTextureInfo* p_unk0x1c); // vtable+0x14
-	virtual void VTable0x18();                           // vtable+0x18
-	virtual void Init();                                 // vtable+0x1c
-	virtual void VTable0x20(undefined4);                 // vtable+0x20
+	virtual MxU32 GetCount();                                                // vtable+0x00
+	virtual void SetCount(MxU32 p_count);                                    // vtable+0x04
+	virtual LegoTextureInfo* GetTextureInfo();                               // vtable+0x08
+	virtual void SetTextureInfo(LegoTextureInfo* p_textureInfo);             // vtable+0x0c
+	virtual LegoTextureInfo* GetCachedTextureInfo();                         // vtable+0x10
+	virtual void SetCachedTextureInfo(LegoTextureInfo* p_cachedTextureInfo); // vtable+0x14
+	virtual void VTable0x18();                                               // vtable+0x18
+	virtual void Init();                                                     // vtable+0x1c
+	virtual void VTable0x20(undefined4);                                     // vtable+0x20
 
 	MxString& GetName() { return m_name; }
 
 private:
-	MxString m_name;            // 0x04
-	undefined4 m_unk0x14;       // 0x14
-	LegoTextureInfo* m_unk0x18; // 0x18
-	LegoTextureInfo* m_unk0x1c; // 0x1c
+	MxString m_name;                      // 0x04
+	MxU32 m_count;                        // 0x14
+	LegoTextureInfo* m_textureInfo;       // 0x18
+	LegoTextureInfo* m_cachedTextureInfo; // 0x1c
 };
 
 #endif // LEGOPHONEME_H
