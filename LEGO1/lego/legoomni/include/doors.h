2e4324c632ac37bab50845b3fbd4f74403992dac
Fabian Neundorf <CommodoreFabianus@gmx.de>
2026-01-11 00:48:56 +0100

    Clear unknowns in `Doors`

--- LEGO1/lego/legoomni/include/doors.h
+++ LEGO1/lego/legoomni/include/doors.h
@@ -10,7 +10,7 @@ class LegoROI;
 // SIZE 0x1f8
 class Doors : public LegoPathActor {
 public:
-	Doors() : m_unk0x154(0), m_ltDoor(NULL), m_rtDoor(NULL), m_unk0x1f4(0) {}
+	Doors() : m_state(0), m_ltDoor(NULL), m_rtDoor(NULL), m_angle(0) {}
 
 	// FUNCTION: LEGO1 0x1000e430
 	// FUNCTION: BETA10 0x100a7f20
@@ -29,19 +29,25 @@ public:
 	void ParseAction(char* p_extra) override;                          // vtable+0x20
 	void Animate(float p_time) override;                               // vtable+0x70
 	MxResult HitActor(LegoPathActor* p_actor, MxBool p_bool) override; // vtable+0x94
-	virtual MxFloat VTable0xcc(float p_time);                          // vtable+0xcc
+	virtual MxFloat CalculateAngle(float p_time);                      // vtable+0xcc
 
 	// SYNTHETIC: LEGO1 0x1000e580
 	// Doors::`scalar deleting destructor'
 
 private:
-	undefined4 m_unk0x154;  // 0x154
-	MxFloat m_unk0x158;     // 0x158
-	LegoROI* m_ltDoor;      // 0x15c
-	LegoROI* m_rtDoor;      // 0x160
-	MxMatrix m_ltDoorLocal; // 0x164
-	MxMatrix m_rtDoorLocal; // 0x1ac
-	MxFloat m_unk0x1f4;     // 0x1f4
+	enum {
+		e_none = 0,
+		e_closed = 1,
+		e_cycling = 2,
+	};
+
+	undefined4 m_state;             // 0x154
+	MxFloat m_hitTime;              // 0x158
+	LegoROI* m_ltDoor;              // 0x15c
+	LegoROI* m_rtDoor;              // 0x160
+	MxMatrix m_ltDoorOriginalLocal; // 0x164
+	MxMatrix m_rtDoorOriginalLocal; // 0x1ac
+	MxFloat m_angle;                // 0x1f4
 };
 
 #endif // DOORS_H
